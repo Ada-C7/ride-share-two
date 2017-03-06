67480f36f8f77c872ee43c8c4f4d8825e1e91e1d
add2point71dots <alcland@gmail.com>
2017-03-06 15:31:39 -0800

    set up Rakefile and spec_helper

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,7 +1,9 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
