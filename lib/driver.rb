fa3f8e7ed0deb362164c7ed30e2feaba14723b20
natalia-ku <bilberryfm@mail.ru>
2017-03-06 15:00:49 -0800

    added initialize ,ethod to Driver class

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,6 +1,8 @@
 module RideShare
   class Driver
+    attr_reader :driver_id, :license, :vin
     def initialize(hash)
+      raise ArgumentError.new("Parameter must be hash only") if hash.class != Hash
       @driver_id = hash[:driver_id]
       @license = hash[:license]
       @vin = hash[:vin]
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -13,7 +13,21 @@ describe "Driver class" do
     driver_hash = {driver_id: 2, license: 3044043, vin: 122189973449}
     RideShare::Driver.new(driver_hash).class.must_equal RideShare::Driver
     end
-  end # end oof initialize
+    it "Raise ArgumentError if passing parameter is not a hash" do
+      proc {
+        RideShare::Driver.new([1,2,3,4,5])
+      }.must_raise ArgumentError
+    end
+    it "Has driver_id, license and vin as initial values" do
+      driver_hash = {driver_id: 2, license: 3044043, vin: 122189973449}
+      driver = RideShare::Driver.new(driver_hash)
+      driver.must_respond_to :driver_id
+      driver.must_respond_to :license
+      driver.must_respond_to :vin
+    end
+
+
+  end # end of initialize method test
 
 
 
