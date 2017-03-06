fc428348d4100af86b6385837dde07fbca39d307
add2point71dots <alcland@gmail.com>
2017-03-06 15:37:01 -0800

    set up initial file structure

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -8,3 +8,6 @@ require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+require_relative '../lib/driver.rb'
+require_relative '../lib/rider.rb'
+require_relative '..lib/trip.rb'
