42449947929a27e2e51b9624ce17b6d980fd598f
Hyunji Kim <ricecakemonster@gmail.com>
2017-03-06 15:55:34 -0800

    Wrote, tested, and passed the first test

--- lib/ride_share_driver.rb
+++ lib/ride_share_driver.rb
@@ -1,1 +1,21 @@
+#Create Rideshare module
+module Rideshare
+#Create Driver class
+  class Driver
+    attr_reader :id, :name, :license_num, :vin
+#an ID, name, license number, vehicle identification number should be initialized\
+    def initialize(args)
+      @id = args[:id]
+      @name = args[:name]
+      @license_num = args[:license_num]
+      @vehicle_id_num = args[:vin]
+    end
+#(vehicle id # should be a specific length to ensure it's a valid vehicle)
 
+#instance_method1 : retrieve the list of trip instances that only this drver has taken
+#instance_method2 : retrieve an average rating for that driver based on all trips taken
+
+#self.method1 : retrive all drivers from the CSV file
+#self.method2 : find a specific driver using their numeric ID
+  end
+end
--- specs/ride_share_dirver_spec.rb
+++ specs/ride_share_dirver_spec.rb
@@ -8,7 +8,8 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "Driver#initialize" do
   it "Creates a Driver class" do
-    driver = Driver.new(id, name, license_num, vehicle_id_num)
-    driver.clasee.must_be_kind_of Rideshare::Driver
+    # args = (id: 1, name: "Bernardo Prosacco", license_num: "HXIK386F3", vin: "WBWSS52P9NEYLVDE9")
+    driver = Rideshare::Driver.new(id: 1, name: "Bernardo Prosacco", license_num: "HXIK386F3", vin: "WBWSS52P9NEYLVDE9")
+    driver.must_be_kind_of Rideshare::Driver
   end
 end
