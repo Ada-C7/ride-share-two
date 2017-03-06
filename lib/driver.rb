a88bfa2e8d747bc8c5a8a9ba23c14d5fbde65f6b
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-06 15:33:31 -0800

    have driver initialization and class method implemented and passing all specs

---
+++ lib/driver.rb
@@ -0,0 +1,28 @@
+require 'csv'
+module RideShare
+  class Driver
+    attr_reader :id, :name, :vin
+
+    def initialize(info)
+      @id = info[:id]
+      @name = info[:name]
+      @vin = info[:vin]
+    end
+
+
+    def self.all(csv_file)
+      drivers = CSV.read(csv_file)
+      drivers.shift
+      drivers.map! do |driver_info|
+        driver = Hash.new
+        driver[:id] = driver_info[0].to_i
+        driver[:name] = driver_info[1]
+        driver[:vin] = driver_info[2]
+        driver_info = driver
+      end
+
+      drivers.map! { |info| self.new(info) }
+      return drivers
+    end
+  end
+end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -7,11 +7,49 @@ require_relative '../lib/driver'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe Driver do
-  describe "Driver#initialize" do
+describe "Driver" do
 
-    it "takes arguments:" do
-      
+  describe "Driver#initialize" do
+    before do
+      driver_info = {
+        id: 1,
+        name: 'Bernardo Prosacco',
+        vin: 'WBWSS52P9NEYLVDE9'
+      }
+      @new_driver = RideShare::Driver.new(driver_info)
+    end
+
+    it "initializes with 1 argument: a driver info hash" do
+      @new_driver.must_be_instance_of RideShare::Driver
+    end
+
+    it "assigns instance variables: id, name, and vin" do
+      @new_driver.id.must_equal 1
+      @new_driver.name.must_equal 'Bernardo Prosacco'
+      @new_driver.vin.must_equal 'WBWSS52P9NEYLVDE9'
+    end
+  end
+
+  describe "Driver#all" do
+
+    # lets work with let instead of before do - end
+    let(:drivers) { RideShare::Driver.all('./support/drivers.csv') }
+
+    # before do
+    #   file = './support/drivers.csv'
+    #   @drivers = RideShare::Driver.all(file)
+    # end
+
+    it "returns an array" do
+      drivers.must_be_instance_of Array
+    end
+
+    it "has instances of drivers in the array" do
+      drivers.each { |driver| driver.must_be_instance_of RideShare::Driver }
+    end
+
+    it "has the same number of driver instances as the csv file" do
+      drivers.length.must_equal 100
     end
   end
 end
