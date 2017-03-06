639dc3f7aad3553d1435bd9dafe112d1b9b01e1a
Tamiko Terada <tamikodevo@gmail.com>
2017-03-06 15:50:48 -0800

    pseudocode edited for trip.rb

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,22 +1,24 @@
 # new Trip class, subclass of RideShare
+# capability to return driver_id or rider_id of trip instance (attr_readers)
 
-# create new trip and take in id, driver_id, rider_id, date, rating as a hash
+# initialize trip
+# take in id, driver_id, rider_id, date, rating as a hash
 @id
 @driver_id
 @rider_id
 @date
 @rating
 
-# capability to return driver_id or rider_id of trip instance (attr_readers)
+# class method: all
+# for each row in CSV file read in and create an instance of trip
+# return all instances of trips
 
 # class method: trips_rode(rider_id)
-# find instances of trips if the rider_id matches
+# find instances of trips where rider_id matches argument
+# alert user if no trips with matching rider_id is found
 # return collection of trips by specific rider
 
 # class method: trips_driven(driver_id)
-# find instances of trips if the driver_id matches
+# find instances of trips where driver_id matches argument
+# alert user if no trips with matching rider_id is found
 # return collection of trips by specific driver
-
-# class method: all
-# read in CSV file for all instances of trips
-# return all instances of trips
