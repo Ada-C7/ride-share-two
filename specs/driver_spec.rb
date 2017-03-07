628f2fd7e9950de129a81998f69f1e96989596f1
Spatterjaaay <aja.pursova@gmail.com>
2017-03-07 13:17:50 -0800

    added test for Driver.all method

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -29,4 +29,34 @@ describe "Driver" do
       driver.must_be_kind_of Driver
     end
   end
+
+  describe "Driver#all" do
+
+    before do
+      @drivers = Driver.all
+    end
+
+    it "Returns an array of all drivers" do
+      @drivers.class.must_equal Array
+      @drivers.each { |driver| driver.must_be_instance_of Driver }
+      @drivers.length.must_equal 100
+
+      @drivers.first.id.must_equal 1
+      @drivers[0].name.must_equal "Bernardo Prosacco"
+      @drivers.first.vin.must_equal "WBWSS52P9NEYLVDE9"
+
+      @drivers.last.id.must_equal 100
+      @drivers[-1].name.must_equal "Minnie Dach"
+      @drivers.last.vin.must_equal "XF9Z0ST7X18WD41HT"
+
+      index = 0
+      CSV.read("support/drivers.csv") do |line|
+
+        @drivers[index].id.must_equal line[0].to_i
+        @drivers[index].name.must_equal line[1].to_s
+        @drivers[index].vin.must_equal line[2]
+        index += 1
+      end
+    end
+  end
 end
