6436e994791a02763dba78d85b5e2ce04a4bfc18
Anacelia Gillette <acgillette426@gmail.com>
2017-03-07 11:22:51 -0800

    Last commit before lunch

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,17 +1,48 @@
 
   class RideShare::Trip
-    attr_reader
+    attr_reader :trip_id, :driver_id, :rider_id, :date,
+    :rating
 
-    def initialize
+    def initialize(trip_data)
+      @trip_id = trip_data[:trip_id]
+      @driver_id = trip_data[:driver_id]
+      @rider_id = trip_data[:rider_id]
+      @date = trip_data[:date]
+      @rating = trip_data[:rating]
+    end
+
+    def driver
+    end
+
+    def trip
     end
 
     def self.all
+      trip_array = []
+      CSV.open("support/trips.csv", 'r').each do |trip|
+        new_trip = RideShare::Trip.new({
+          :trip_id => trip[0],
+          :driver_id => trip[1],
+          :rider_id => trip[2],
+          :date => trip[3],
+          :rating => trip[4]
+          })
+          trip_array << new_trip
+      end
+      trip_array.shift
+      return trip_array
     end
 
-    def self.all_drivers
+    def self.all_drivers(id)
+      trips = RideShare::Trip.all
+      drivers = trips.select { |t| t.driver_id == id }
+      return drivers
     end
 
-    def self.all_riders
+    def self.all_riders(id)
+      trips = RideShare::Trip.all
+      riders = trips.select { |t| t.rider_id == id }
+      return riders
     end
 
   end
--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,1 +1,74 @@
 require_relative 'spec_helper'
+
+describe "Trip Class" do
+
+  describe "Initialize Method" do
+
+    let(:trip) {
+        RideShare::Trip.new({
+        :trip_id => "1",
+        :driver_id => "1",
+        :trip_id => "1234",
+        :date => "2017-04-07",
+        :rating => "5"
+        })
+      }
+
+
+    it "Initializes an object" do
+      trip.must_be_instance_of RideShare::Trip
+    end
+
+    it "Initializes with an ID, name, and phone number" do
+      trip.must_respond_to :trip_id
+      trip.must_respond_to :driver_id
+      trip.must_respond_to :trip_id
+      trip.must_respond_to :date
+      trip.must_respond_to :rating
+    end
+  end
+
+  describe "self.all" do
+
+    let(:trips) {RideShare::Trip.all }
+
+    it "Returns an array" do
+      trips.must_be_instance_of Array
+    end
+
+    it "Returns an Array with elements" do
+      trips.length.must_be :>, 0
+    end
+
+    it "Returns an array of trip Objects" do
+      trips.each do |trip|
+        trip.must_be_instance_of RideShare::Trip
+      end
+    end
+
+    it "Each trip object contains an id, name and vin" do
+      trips.each do |trip|
+        trip.must_respond_to :trip_id
+        trip.must_respond_to :driver_id
+        trip.must_respond_to :trip_id
+        trip.must_respond_to :date
+        trip.must_respond_to :rating
+      end
+    end
+
+    it "Contains the correct information" do
+      first = trips[0]
+      first.trip_id.must_equal "1"
+      first.driver_id.must_equal "1"
+      first.date.must_equal "2016-04-05"
+    end
+  end
+
+  describe "self.all_drivers" do
+  end
+
+  describe "self.all_riders" do
+  end 
+
+
+end
