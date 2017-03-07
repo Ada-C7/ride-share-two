90026256a10f09e708f6376e296a00e24f3aa865
janicewilson <janice.wilson@mac.com>
2017-03-07 12:59:20 -0800

    Updated spec_helper.rb to include rider.rb and trip.rb. Created tests for Rider#initialize.

--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -1,13 +1,31 @@
-require 'simplecov'
-SimpleCov.start
-
-require 'minitest/autorun'
-require 'minitest/reporters'
-require_relative '../lib/rider'
-require_relative '../lib/trip'
-
-Minitest::Reporters.use!
-Minitest::Reporters::SpecReporter.new
+require_relative 'spec_helper'
 
 describe Rider do
+
+     before do
+          @id = 7
+          @name = "Han"
+          @phone = "777-777-7777"
+          @han = Rider.new(@id, @name, @phone)
+     end
+
+     describe "Rider#initialize" do
+
+          it "Creates class Rider:" do
+            @han.must_be_kind_of Rider
+          end
+
+          it "Takes ID, name and phone:" do
+            @han.must_respond_to :id
+            @han.id.must_equal @id
+
+            @han.must_respond_to :name
+            @han.name.must_equal @name
+
+            @han.must_respond_to :phone
+            @han.phone.must_equal @phone
+          end
+
+     end
+
 end
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -11,3 +11,5 @@ require 'csv'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative '../lib/driver'
+require_relative '../lib/rider'
+require_relative '../lib/trip'
