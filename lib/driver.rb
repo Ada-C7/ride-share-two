6476bd4fc405b9801b9026aa1173fbc2bf15faba
Spatterjaaay <aja.pursova@gmail.com>
2017-03-07 13:18:16 -0800

    added code for Driver.all method

--- lib/driver.rb
+++ lib/driver.rb
@@ -9,25 +9,34 @@ class Driver
     @vin = vin
   end
 
-def self.all
-  # read SCV
-  # returns a list of driver instances
-end
+  def self.all
 
-def self.find(driver_id)
-  # searches .all for all driver matching parameter
-  # returns an instance of a driver
-end
+    drivers = CSV.read("support/drivers.csv", { :headers => true })
+    drivers_array = []
 
-def lists_trips
-  # passes driver ID to find_trips_per_driver
-  # returns a list of trip instances only this driver has taken
-end
+    drivers.each do |line|
+      drivers_array << Driver.new(line[0].to_i, line[1], line[2])
+    end
 
-def avg_rating
-  # accesses rating form each trip instance
-  # calcualtes average
-  # returns an average rating for that driver based on all trips taken
-end
+    return drivers_array
+    # read SCV
+    # returns a list of driver instances
+  end
+
+  def self.find(driver_id)
+    # searches .all for all driver matching parameter
+    # returns an instance of a driver
+  end
+
+  def lists_trips
+    # passes driver ID to find_trips_per_driver
+    # returns a list of trip instances only this driver has taken
+  end
+
+  def avg_rating
+    # accesses rating form each trip instance
+    # calcualtes average
+    # returns an average rating for that driver based on all trips taken
+  end
 
 end
