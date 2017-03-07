9cef6259f1a77900bac3be89abca4e02915b8b58
Tamiko Terada <tamikodevo@gmail.com>
2017-03-06 23:05:45 -0800

    specs edited: driver_spec.rb wording edited

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -4,24 +4,23 @@ require 'minitest/skip_dsl'
 
 require_relative '../lib/driver'
 
-describe "RideShare::Trip" do
-
+describe "RideShare::Driver" do
   before do
     #9,Simone Hackett,4RA34A5K3YPN8H5P4
     @driver = RideShare::Driver.new({id: "9", name: "Simone Hackett", vin: "4RA34A5K3YPN8H5P4"})
   end
 
-  describe "Trip#initialize" do
+  describe "Driver#initialize" do
     it "creates a new instance of trip" do
       @driver.must_be_instance_of RideShare::Driver
     end
-    it "passes in the driver name" do
+    it "passes in the driver id" do
       @driver.id.must_equal "9"
     end
-    it "passes in the vin" do
+    it "passes in the driver name" do
       @driver.name.must_equal "Simone Hackett"
     end
-    it "passes in the rider_id" do
+    it "passes in the vin" do
       @driver.vin.must_equal "4RA34A5K3YPN8H5P4"
     end
     it "can create a new driver with a missing vin" do
