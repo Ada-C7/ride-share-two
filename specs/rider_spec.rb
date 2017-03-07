76c6e87ad03e40336bb96ba17c94918bcf57c4e0
Spatterjaaay <aja.pursova@gmail.com>
2017-03-07 14:01:05 -0800

    added tests for Rider.all

--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -10,7 +10,7 @@ describe "Rider" do
       id = 12345
       phone_num = "(206) 222 222"
       rider = Rider.new(id, name, phone_num)
-      
+
       rider.must_respond_to :id
       rider.id.must_equal id
 
@@ -30,4 +30,36 @@ describe "Rider" do
       rider.must_be_kind_of Rider
     end
   end
+
+  describe "Rider#all" do
+
+    before do
+      @riders = Rider.all
+    end
+
+    it "Returns an array of all riders" do
+      @riders.class.must_equal Array
+      @riders.each { |rider| rider.must_be_instance_of Rider }
+      @riders.length.must_equal 300
+
+      @riders.first.id.must_equal 1
+      @riders[0].name.must_equal "Nina Hintz Sr."
+      @riders.first.phone_num.must_equal "560.815.3059"
+
+      @riders.last.id.must_equal 300
+      @riders[-1].name.must_equal "Miss Isom Gleason"
+      @riders.last.phone_num.must_equal "791-114-8423 x70188"
+
+      index = 0
+      CSV.read("support/riders.csv") do |line|
+
+        @riders[index].id.must_equal line[0].to_i
+        @riders[index].name.must_equal line[1]
+        @riders[index].phone_num.must_equal line[2]
+        index += 1
+      end
+    end
+
+  end
+
 end
