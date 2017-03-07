085b25afc73c99b182a8fc81108c15042d29e59b
Spatterjaaay <aja.pursova@gmail.com>
2017-03-06 16:34:22 -0800

    added bare testing structure

---
+++ specs/driver_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+require_relative '../lib/driver'
+
+describe "Driver" do
+
+  describe "#initialize" do
+  end
+
+end
---
+++ specs/trip_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+require_relative '../lib/trip'
+
+describe "Trip" do
+
+  describe "#initialize" do
+  end
+
+end
