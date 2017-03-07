f8f7fe0c9c056e79340143d0f35abc88387013c9
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-07 11:06:24 -0800

    Set up Rakefile, spec_helper, ride_share.rb, and ran initial tests

---
+++ ride_share.rb
@@ -0,0 +1,7 @@
+require 'csv'
+
+module RideShare; end
+
+require_relative 'lib/drivers.rb'
+require_relative 'lib/trips.rb'
+require_relative 'lib/riders.rb'
---
+++ specs/drivers_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe 'tests' do
+  it "should run tests" do
+    true.must_equal false
+  end
+
+  it "should have access to drivers.rb" do
+    TEST_CONTSANT.must_equal 11
+  end
+end
