47dea5aa83863ea61c9dfdc15e73188c6d2b4655
janicewilson <janice.wilson@mac.com>
2017-03-07 13:00:08 -0800

    Updated spec_helper.rb to include rider.rb and trip.rb. Created tests for Rider#initialize.

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,4 +1,60 @@
 require_relative 'trip'
 
 class Rider
+
+     attr_reader :id, :name, :phone
+     attr_accessor :trips, :average_rating
+
+     def initialize(id, name, phone)
+          @id = id
+          @name = name
+          @phone = phone
+          #@trips = recall_trips
+          #@average_rating = calculate_average_rating
+     end
+
+     # #retrieve the list of trip instances that only this driver has taken
+     # def recall_trips(@id)
+     #      #TODO Build block after trip.rb has been completed.
+     #      #Calls Trip class method to retrieve list of trip instances according to @id.
+     #      #@trips = Trip.find(@id)
+     #      #return @trips
+     # end
+     #
+     # #retrieve an average rating for that driver based on all trips taken
+     # def calculate_average_rating(@id)
+     #      #TODO Build block after trip.rb has been completed.
+     #      all_trips = recall_trips(@id)
+     #      @average_rating = all_trips.rating.map { | sum, rating | sum + rating }/all_trips.length
+     #      return @average_rating
+     # end
+     #binding.pry
+
+     # def self.all(file)
+     #      all_drivers = []
+     #
+     #      CSV.open(file).each do | line |
+     #           @id = line[0].to_i
+     #           @name = line[1]
+     #           @vin = line[2]
+     #           driver = Driver.new(@id, @name, @vin)
+     #           all_drivers << driver
+     #      end
+     #
+     #      return all_drivers
+     # end
+     #
+     # def self.find(file, id)
+     #
+     #      Driver.all(file).each do | account |
+     #
+     #           if account.id == id
+     #                return account
+     #           end
+     #
+     #      end
+     #
+     #      raise ArgumentError.new "Driver #{id} does not exist."
+     #
+     # end
 end
