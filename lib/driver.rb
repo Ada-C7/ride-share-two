013a2ccce7815573a67aff53c01fffa24e709841
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-07 13:16:06 -0800

    added test and argument error to make sure driver vin is 17 characters. also added all class method for Driver that reads in a csv file and returns a list of all drivers.

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,11 +1,27 @@
+require 'csv'
+require 'pry'
+
 class Driver
   attr_reader :id, :name, :vin
 
   def initialize(id, name, vin)
     @id = id
     @name = name
+    raise ArgumentError.new("vin must be 17 characters") if vin.length != 17
     @vin = vin
+  end
 
+  def self.all
+    drivers = []
+    # binding.pry
+    CSV.foreach("./support/drivers.csv", {:headers => true}).each do |line|
+      id = line[0]
+      name = line[1]
+      vin = line[2]
+
+      drivers << Driver.new(id, name, vin)
+    end
+    return drivers
   end
 
 end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/driver'
 describe "Driver tests" do
   describe "Driver#initialize" do
     it "takes an id, name, and vin" do
-        new_driver = Driver.new("3", "Bob Jones", "12345")
+        new_driver = Driver.new("3", "Bob Jones", "L1CXMYNZ3MMGTTYWU")
 
         new_driver.must_respond_to :id
         new_driver.id.must_equal "3"
@@ -13,8 +13,35 @@ describe "Driver tests" do
         new_driver.name.must_equal "Bob Jones"
 
         new_driver.must_respond_to :vin
-        new_driver.vin.must_equal "12345"
+        new_driver.vin.must_equal "L1CXMYNZ3MMGTTYWU"
     end
+
+    it "raises an error if vin is not 17 characters long" do
+
+      proc {Driver.new("3", "Bob Jones", "12345")
+      }.must_raise ArgumentError
+
+    end
+
+  end
+
+
+  describe "Driver.all" do
+    it "Returns an array of all drivers" do
+      drivers_array = Driver.all
+
+      drivers_array.class.must_equal Array
+
+    end
+
+    it "Everything in array is a Driver" do
+      drivers_array = Driver.all
+
+      drivers_array.each do |element|
+        element.must_be_instance_of Driver
+      end
+    end
+
   end
 
 end
