66a1908299eaf095a26629f85703be4d067fc1e1
Ashtn <ashtonehorton@gmail.com>
2017-03-06 16:41:37 -0800

    puesdocode for rider methods written

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,3 +1,28 @@
 class Driver
 
+
+
+#def trips_taken
+#return list of trips taken by an instance of a diver
+#end
+
+
+#def average_rating
+#average_rating = ratings.all / ratings.length 
+#return average_rating
+#end
+
+#self.all
+#reads csv file
+#creates new dirvers instances
+#returns array/hash of all drivers
+#@@drivers <<
+#end
+
+#self.find_driver(id)
+#find id in @@drivers
+#returns a single driver associated with that id
+#end
+
+
 end
