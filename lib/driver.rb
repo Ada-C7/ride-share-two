5df8584badf2ccb4897eeed09b01a17de5857e37
Ann Dai <gofarann@gmail.com>
2017-03-06 16:46:52 -0800

    added stubs for driver class. created init method.

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,6 +1,30 @@
 require 'csv'
 
 module Rideshare
+
   class Driver
+    attr_reader :id, :name, :vin
+
+    def initialize driver_hash
+      @id = driver_hash[:id]
+      @name = driver_hash[:name]
+      @vin = driver_hash[:vin]
+    end
+
+    def trips driver_id
+
+    end
+
+    def all
+      
+    end
+
+    def find driver_id
+    end
+
+    def rating driver_id
+    end
+
+
   end
 end
