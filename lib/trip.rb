8adf60d3a837a754c59714a47daeb6bdb5600616
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-03-07 12:06:56 -0800

    tests and intitialize methods for all three classes, plus driver::self.all_drivers

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,14 +1,24 @@
 module RideShareTwo
   class Trip
+
     attr_reader :trip_id, :rider_id, :driver_id, :date, :rating
+
     def initialize(trip_id, rider_id, driver_id, date, rating)
       @trip_id = trip_id
       @rider_id = rider_id
       @driver_id = driver_id
       @date = date
       @rating = rating
-      # raise ArgumentError.new("rating must be >= 5") if rating < 5
+        raise ArgumentError.new("rating must be 1-5") if rating >= 6 || rating <= 0
     end
+
+    # def trip_driver
+    #   return driver_id #fix - must return instance, use find_driver in driver
+    # end
+    #
+    # def trip_rider
+    #   return rider_id #fix - must return instance, use
+    # end
 
   end
 
--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 
-describe "Trip_initialize" do
+describe "Trip Initialize" do
 
     it "Takes an ID, Rider ID, Driver ID, date, and a rating" do
       trip_id = 1337
@@ -25,4 +25,51 @@ describe "Trip_initialize" do
       trip.must_respond_to :rating
       trip.rating.must_equal rating
     end
+
+    it "Raises an ArgumentError if the rating is greater than 5" do
+      proc {
+        RideShareTwo::Trip.new(1337, 318, 54, "date", 6)
+      }.must_raise ArgumentError
+    end
+
+    it "Raises an ArgumentError if the rating is 0" do
+      proc {
+        RideShareTwo::Trip.new(1337, 318, 54, "date", 0)
+      }.must_raise ArgumentError
+    end
+
+    it "Raises an ArgumentError if the rating is less than 0" do
+      proc {
+        RideShareTwo::Trip.new(1337, 318, 54, "date", -3)
+      }.must_raise ArgumentError
+    end
+
 end
+
+# from here below, these are ng
+
+# describe "Trip_Driver" do
+#     it "Retrieves the associated trip driver instance through the driver ID" do
+#       trip_id = 1337
+#       rider_id = 318
+#       driver_id = 54
+#       date = "date"
+#       rating = 4
+#       trip = RideShareTwo::Trip.new(trip_id, rider_id, driver_id, date, rating)
+#
+#       trip.driver_id.must_equal 54
+#     end
+# end
+#
+# describe "Trip_Rider" do
+#     it "Retrieves the associated trip rider instance through the rider ID" do
+#       trip_id = 1337
+#       rider_id = 318
+#       driver_id = 54
+#       date = "date"
+#       rating = 4
+#       trip = RideShareTwo::Trip.new(trip_id, rider_id, driver_id, date, rating)
+#
+#       trip.rider_id.must_equal 318
+#     end
+# end
