9ad8fa52fb0cb3454624107e9965925a2c207aaa
janicewilson <janice.wilson@mac.com>
2017-03-06 20:09:51 -0800

    Made spec_helper magic happen. Wrote pseudo code for two Driver methods. Wrote test to ensure CSV file is read correctly

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,14 +1,46 @@
 require_relative 'trip'
 
+
 class Driver
 
      attr_reader :id, :name, :vin
+     attr_accessor :trips, :average_rating
 
      def initialize(id, name, vin)
           raise ArgumentError if vin.class != String || vin.length > 17
           @id = id
           @name = name
           @vin = vin
+          #@trips = recall_trips
+          #@average_rating = calculate_average_rating
      end
 
+     # #retrieve the list of trip instances that only this driver has taken
+     # def recall_trips(@id)
+     #      #TODO Build block after trip.rb has been completed.
+     #      #Calls Trip class method to retrieve list of trip instances according to @id.
+     #      #@trips = Trip.find(@id)
+     #      #return @trips
+     # end
+     #
+     # #retrieve an average rating for that driver based on all trips taken
+     # def calculate_average_rating(@id)
+     #      #TODO Build block after trip.rb has been completed.
+     #      all_trips = recall_trips(@id)
+     #      @average_rating = all_trips.rating.map { | sum, rating | sum + rating }/all_trips.length
+     #      return @average_rating
+     # end
+
+     # def self.all(file)
+     #      all_accounts = []
+     #      CSV.open(file).each do | line |
+     #           id = line[0].to_i
+     #           balance = line[1].to_i
+     #           date_opened = line[2]
+     #           account = Account.new(id, balance, date_opened)
+     #           all_accounts << account
+     #      end
+     #
+     #      return all_accounts
+     # end
 end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -1,13 +1,4 @@
-require 'simplecov'
-SimpleCov.start
-
-require 'minitest/autorun'
-require 'minitest/reporters'
-require_relative '../lib/driver'
-require_relative '../lib/trip'
-
-Minitest::Reporters.use!
-Minitest::Reporters::SpecReporter.new
+require_relative 'spec_helper'
 
 describe Driver do
 
@@ -18,13 +9,13 @@ describe Driver do
           @leia = Driver.new(@id, @name, @vin)
      end
 
-     describe "Driver#initialization" do
+     describe "Driver#initialize" do
 
           it "Creates class Driver" do
             @leia.must_be_kind_of Driver
           end
 
-          it "Takes ID, name and vehicle ID" do
+          it "Takes ID, name and VIN" do
             @leia.must_respond_to :id
             @leia.id.must_equal @id
 
@@ -35,12 +26,47 @@ describe Driver do
             @leia.vin.must_equal @vin
           end
 
-          it "Verifies vehicle ID is a string, containing no more than 17 characters" do
+          it "Verifies VIN is a string, containing no more than 17 characters" do
             proc {Driver.new(@id, @name, 777)
             }.must_raise ArgumentError
 
             proc {Driver.new(@id, @name, "777333777333777333")
             }.must_raise ArgumentError
+          end
+     end
+
+     describe "Self#all" do
+
+          before do
+               @file = "../support/drivers.csv"
+               @all_drivers = Driver.all(@file)
+          end
+
+          describe "Driver.all" do
+              it "Returns an array of all drivers" do
+                   @all_drivers.must_be_kind_of Array
+               end
+
+              it "The number of drivers is correct" do
+                   lines = File.foreach(@file).count
+                   @all_drivers.length.must_equal lines
+               end
+
+               it "Everything in the array is a Driver" do
+                   @all_drivers.each do | driver |
+                       driver.must_be_kind_of Driver
+                    end
+               end
+
+              it "The ID, name and VIN of the first and last drivers match the CSV file" do
+                   @all_drivers[0].id.must_equal IO.readlines(@file).first[0]
+                   @all_drivers[0].name.must_equal IO.readlines(@file).first[1]
+                   @all_drivers[0].vin.must_equal IO.readlines(@file).first[2]
+
+                   @all_drivers[0].id.must_equal IO.readlines(@file).last[0]
+                   @all_drivers[0].name.must_equal IO.readlines(@file).last[1]
+                   @all_drivers[0].vin.must_equal IO.readlines(@file).last[2]
+               end
           end
      end
 end
