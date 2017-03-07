51dd26f3baea546cbbdf7019c60cb3a04b9d2f9e
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-03-07 14:49:19 -0800

    self.all_drivers and self.find_driver methods

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 module RideShareTwo
   class Rider
 
@@ -8,6 +10,20 @@ module RideShareTwo
       @name = name
       @phone_num = phone_num
     end
+
+    def self.all_riders
+      all_riders = []
+      CSV.open("support/riders.csv",{:headers => true}).each do |array|
+        all_riders << self.new(array[0], array[1], array[2].to_s)
+      end
+      return all_riders
+    end
+
+    def self.find_rider(rider_id)
+      result = self.all_riders.select { |a| a.rider_id == rider_id }
+      return result.first
+    end
+
 
   end
 
