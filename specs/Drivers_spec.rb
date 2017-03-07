af04c38948396f7de2af1520cc7f82f67e387e9c
Olivia <ssecretsharer@gmail.com>
2017-03-07 13:01:08 -0800

    First commit of Drivers and its spec. Stubs are in, two methods are written, two are psudocoded, also adding rake changes and spec helper for this assingment

---
+++ specs/Drivers_spec.rb
@@ -0,0 +1,94 @@
+
+require_relative 'spec_helper'
+
+describe "Driver Class" do
+
+  # before do
+  #   @name
+  #   @id
+  #   @vehichle_id
+  # end
+
+  describe "self.all" do
+
+    it "returns an array"do
+
+    end
+    it "all elements of array should be drivers" do
+
+    end
+    it "includes first data line" do
+
+    end
+    it "includes last data line" do
+
+    end
+    it "includes line 50 of array" do
+
+    end
+
+
+
+  describe "driver_trips(trips)" do
+
+    it "returns an array" do
+    end
+    it "all elements of array should be trip instances" do
+    end
+    it "each trip should have an driver_ID that matches the driver_ID that i'm in. "
+  end
+
+
+
+  describe "driver_rating" do
+
+    it "Should return an integer" do
+
+    end
+
+    it "Driver ratings should correspond to trips taken by that driver" do
+      #driver ID should match trip/riderID, see driver trips
+    end
+
+
+  end
+
+
+
+  describe "self.find" do
+    it "Should return an instance of driver"do
+
+    end
+    it "Driver ID should return an integer"do
+
+    end
+    it "Should raise ArgumentError if search doesn't
+    return a match" do
+
+    end
+
+  end
+
+
+
+  end
+
+
+  # it "this is is an instance of a driver" do
+  #   driver = Driver.new
+  #   driver.must_be_instance_of Driver
+  # end
+  #
+  # it "driver ID must be an integer" do
+  #   driver = Driver.new
+  #   driver.id = 1
+  #   driver.id.must_be_instance_of Fixnum
+  # end
+  #
+  # if driver should be a hash
+  #   driver [1] [name, num]
+  #   driver [100] [name, num]
+
+
+
+end
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,7 +1,13 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
+
+require_relative '../lib/Drivers.rb'
+require_relative '../lib/Riders.rb'
+require_relative '../lib/Trips.rb'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
