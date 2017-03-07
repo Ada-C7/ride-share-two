a5aa5649e398163f26baaa03b1c89a2aad0e345d
Sahana Murthy <s.murthy730.com>
2017-03-06 16:07:08 -0800

    Created passing tests for Driver class initialize method Argument Errors

--- lib/driver.rb
+++ lib/driver.rb
@@ -4,8 +4,12 @@ module RideShare
 
     def initialize(args)
       @id = args[:id]
+      raise ArgumentError.new("driver id must be an integer") unless @id.class == Integer
       @name = args[:name]
+      raise ArgumentError.new("driver name must be a string") unless @name.class == String
       @vehicle_id = args[:vehicle_id]
+      raise ArgumentError.new("vehicle id must be a string") unless @vehicle_id.class == String
+      raise ArgumentError.new("vehicle id must be 17 characters long") unless @vehicle_id.length == 17
     end
   end
 end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -25,14 +25,36 @@ describe "Driver" do
       @driver.id.must_equal @args[:id]
     end
 
+    it "Raises Argument Error if driver id is not an integer" do
+      proc {
+        RideShare::Driver.new(id: "2", name: "Sahana", vehicle_id: "WBWSS52P9NEYLVDE9")
+      }.must_raise ArgumentError
+    end
+
     it "Takes a driver name" do
       @driver.must_respond_to :name
       @driver.name.must_equal @args[:name]
     end
 
+    it "Raises Argument Error if driver name is not a string" do
+      proc {
+        RideShare::Driver.new(id: 2, name: 65, vehicle_id: "WBWSS52P9NEYLVDE9")
+      }.must_raise ArgumentError
+    end
+
     it "Takes a vehicle identification number" do
       @driver.must_respond_to :vehicle_id
       @driver.vehicle_id.must_equal @args[:vehicle_id]
+    end
+
+    it "Raises Argument Error if given incorrect vehicle id" do
+      proc {
+        RideShare::Driver.new(id: 2, name: "Sahana", vehicle_id: "765")
+      }.must_raise ArgumentError
+
+      proc {
+        RideShare::Driver.new(id: 2, name: "Sahana", vehicle_id: 765)
+      }.must_raise ArgumentError
     end
   end
 end
