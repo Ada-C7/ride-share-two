9b81bbec40a636787797243d33ec4d96b90efd48
AlisonZ <alisonkzerbe@gmail.com>
2017-03-06 16:56:05 -0800

    adds the pseudocode for driver and sets up intialization which is not working right now

---
+++ lib/driver.rb
@@ -0,0 +1,49 @@
+#require csv??
+#require anything else?
+module RideShare
+    attr_reader :id, :name, :vin
+    class Driver(id, name, vin)
+        #hmmm.. this isn't working
+        @id = id
+        @name = name
+        #vin should be of a certain length and needs to be checked
+        @vin = vin
+    end
+
+#find_trips(id)
+    #retrieve list of all trip instances based on the drivers id
+    #do this by calling the driver_find_all in the Trips class
+
+    #return the list of trips for this individual driver
+#end
+
+
+#average_rating(id)
+    #call the find_trips(id) which will call the driver_find_all or just call driver find all
+
+    #start a ratings counter at 0
+    #loop through the array of all trips for this driver
+    #at each loop add the rating to the ratings counter
+    #divide total ratings by the length of the trips array
+
+    #method returns the average rating
+# end
+
+
+#self.all
+    #reads in the csv
+    #stores each line in the CSV as an element in an array
+# end
+
+#self.find(id)
+    #calls self.all
+    #uses the array of trips from all and checks the array.id to see if it matches the id argument passed in
+    #if it does, then return this instance of driver
+# end
+
+
+
+
+
+
+end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -1,2 +1,14 @@
 require_relative 'spec_helper'
 require_relative '../lib/driver.rb'
+
+
+describe "Driver" do
+    describe "initialize method" do
+        it "Driver.new must be an instance of a driver" do
+            @my_driver = RideShare::Driver.new(16, "Travis Bickle", 123456)
+            @my_driver.must_be_instance_of RideShare::Driver
+        end
+
+    end
+
+end
