9e55409d5a324153889c2dc8674cd08cda3c7b71
Tamiko Terada <tamikodevo@gmail.com>
2017-03-06 23:19:21 -0800

    method added: driver#all method created to read CSV

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 # new Driver, subclass of RideShare
 module RideShare
   class Driver
@@ -8,17 +10,29 @@ module RideShare
       @name = driver_info[:name]
       @vin = driver_info[:vin]
     end
+
+    # instance method: trips
+    # return collection of trip instances by this driver
+
+    # instance method: avg-rating
+    # return rounded average rating across all trips by this driver
+
+    # class method: all
+    def self.all
+      @all_drivers = []
+      # read in CSV file for all instances of drivers
+      CSV.foreach("support/drivers.csv", {:headers => true}) do |row| # file directory for rake
+        @all_drivers << RideShare::Driver.new({
+          id: row[0],
+          name: row[1],
+          vin: row[2]
+        })
+      end
+      return @all_drivers
+      # return all instances of driver
+    end
+    # class method: find(id)
+    # return specific instance of driver (previously instantiated)
+
   end
 end
-# instance method: trips
-# return collection of trip instances by this driver
-
-# instance method: avg-rating
-# return rounded average rating across all trips by this driver
-
-# class method: all
-# read in CSV file for all instances of drivers
-# return all instances of driver
-
-# class method: find(id)
-# return specific instance of driver (previously instantiated)
