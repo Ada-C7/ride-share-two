bc8fb1cca68d4275aeec1c79d951d7a3375cead8
Hyunji Kim <ricecakemonster@gmail.com>
2017-03-07 10:48:26 -0800

    Wrote and passed the third test.

--- lib/ride_share_driver.rb
+++ lib/ride_share_driver.rb
@@ -1,3 +1,4 @@
+require 'csv'
 #Create Rideshare module
 module Rideshare
 #Create Driver class
@@ -9,14 +10,31 @@ module Rideshare
       @name = args[:name]
       @license_num = ""
       @vin = args[:vin]
+      #(vehicle id # should be a specific length to ensure it's a valid vehicle)
       raise ArgumentError.new("Not a valid vin number") if @vin.length > 17
     end
-#(vehicle id # should be a specific length to ensure it's a valid vehicle)
-
-#instance_method1 : retrieve the list of trip instances that only this drver has taken
-#instance_method2 : retrieve an average rating for that driver based on all trips taken
 
 #self.method1 : retrive all drivers from the CSV file
+    def self.all
+      csv = []
+      CSV.read("./support/drivers.csv").drop(1).each do |driver|
+        csv.push({id: driver[0].to_i, name: driver[1], vin: driver[2]})
+      end
+      return csv
+    end
 #self.method2 : find a specific driver using their numeric ID
+    def self.find
+
+    end
+#instance_method1 : retrieve the list of trip instances that only this drver has taken
+    def all_trips
+    end
+#instance_method2 : retrieve an average rating for that driver based on all trips taken
+    def average_rating
+    end
+
   end
 end
+
+# Rideshare::Driver.all
+# Rideshare::Driver.new(id: 1, name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
--- specs/ride_share_dirver_spec.rb
+++ specs/ride_share_dirver_spec.rb
@@ -2,19 +2,49 @@ require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/reporters'
+# require 'minitest/pride'
 require 'minitest/skip_dsl'
-require_relative '../lib/ride_share_driver.rb'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe "Driver#initialize" do
-  it "Creates a Driver class" do
-    args = {id: 1, name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9"}
-    driver = Rideshare::Driver.new(args)
-    driver.must_be_kind_of Rideshare::Driver
+require_relative '../lib/ride_share_driver.rb'
+
+describe "Driver" do
+  # let (:driver) {driver = Rideshare::Driver.new(args)}
+
+  describe "Driver#initialize" do
+    it "Creates a Driver class" do
+      args = {id: 1, name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9"}
+      driver = Rideshare::Driver.new(args)
+      driver.must_be_kind_of Rideshare::Driver
+    end
+
+    it "Raise an argument error if vin number is longer than 17" do
+      args = {id: 1, name: "Bernardo Prosacco", vin: "WBWSS5245P9NEYLVDE29"}
+      proc {Rideshare::Driver.new(args)}.must_raise ArgumentError
+    end
   end
 
-  it "Raise an argument error if vin number is longer than 17" do
-    args = {id: 1, name: "Bernardo Prosacco", vin: "WBWSS5245P9NEYLVDE29"}
-    proc {Rideshare::Driver.new(args)}.must_raise ArgumentError
+  describe "Driver.all" do
+    it "Retrieve all drivers from the CSV file" do
+      Rideshare::Driver.all.length.must_equal 100
+    end
   end
+
+  # describe "Driver.find" do
+  #   it do
+  #
+  #   end
+  # end
+  #
+  # describe "Driver#all_trips" do
+  #   it do
+  #
+  #   end
+  # end
+  #
+  # describe "Driver#average_rating" do
+  #   it do
+  #
+  #   end
+  # end
 end
