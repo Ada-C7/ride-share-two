f463fa830ddc3f48d33490e047aad1bbe33ba2f0
madamecurie <usermonalisa@gmail.com>
2017-03-07 11:36:23 -0800

    Wrote pseudocode for driver class

--- lib/driver.rb
+++ lib/driver.rb
@@ -3,9 +3,34 @@ require_relative 'invalid_vin_error'
 
 module RideShare
   class Driver
+    def initialize (driver_id, name, vin) #Instantiated Driver class with ID, name and vehicle identification number
+      @id = id
+      @name = name
+      @vin = vin #check vehicle identification number should be a specific length else raise InvalidVinError
+    end
 
+    def trips
+      #Call trips_driver method on class Trip with argument of driver_id to return an array of trips that this driver has completed
+    end
+
+    def average_rating
+      #Collect all trip instances and then loop through each to find total rating and divide it by length of trip instances to get average
+    end
+
+
+    def self.all
+      #Use CSV.read method passing file name as argument
+    end
+
+    def self.find driver_id
+      #check if driver_id is included in the list of drivers found in all method above
+    end
 
 
 
   end
+
+
+
+
 end
