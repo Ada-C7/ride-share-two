a959e5fc8c09ea726274eab722a36dc8a5e6555e
Cara Comfort <cara.comfort10@gmail.com>
2017-03-07 11:26:23 -0800

    Added InvalidVinError class

---
+++ lib/invalid_vin_error.rb
@@ -0,0 +1,4 @@
+module RideShare
+  class InvalidVinError < StandardError
+  end
+end
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -18,3 +18,4 @@ SimpleCov.start
 require_relative '../lib/driver'
 require_relative '../lib/rider'
 require_relative '../lib/trip'
+require_relative '../lib/invalid_vin_error'
