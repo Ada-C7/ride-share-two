1b336636fbba90f7b9437e379024307ab53e3798
add2point71dots <alcland@gmail.com>
2017-03-07 10:39:09 -0800

    raise argument errors for invalid information in initialize (and test that)

--- lib/driver.rb
+++ lib/driver.rb
@@ -2,6 +2,10 @@ class Driver
   attr_reader :id, :name, :vin
 
   def initialize(driver_hash)
+    raise ArgumentError.new("Driver id must be an integer") if driver_hash[:id].class != Integer
+    raise ArgumentError.new("Name must be a non-empty string") if driver_hash[:name].class != String || driver_hash[:name] == ""
+    raise ArgumentError.new("VIN must be a non-empty string") if driver_hash[:vin].class != String || driver_hash[:vin] == ""
+
     @id = driver_hash[:id]
     @name = driver_hash[:name]
     @vin = driver_hash[:vin]
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -14,5 +14,26 @@ describe "Driver tests" do
       driver.must_respond_to :vin
       driver.vin.must_equal "1XKAD49X2DJ395724"
     end
+
+    it "only accepts integer IDs" do
+      driver_hash = { id: "id", name: "Ada", vin: "1XKAD49X2DJ395724" }
+      proc { Driver.new(driver_hash) }.must_raise ArgumentError
+    end
+
+    it "only accepts non-empty strings for names" do
+      driver_hash1 = { id: 4, name: "", vin: "1XKAD49X2DJ395724" }
+      driver_hash2 = { id: 4, name: 45, vin: "1XKAD49X2DJ395724" }
+
+      proc { Driver.new(driver_hash1) }.must_raise ArgumentError
+      proc { Driver.new(driver_hash2) }.must_raise ArgumentError
+    end
+
+    it "only accepts non-empty strings for VINs" do
+      driver_hash1 = { id: 4, name: "Ada", vin: "" }
+      driver_hash2 = { id: 4, name: "Ada", vin: [] }
+
+      proc { Driver.new(driver_hash1) }.must_raise ArgumentError
+      proc { Driver.new(driver_hash2) }.must_raise ArgumentError
+    end
   end
 end
