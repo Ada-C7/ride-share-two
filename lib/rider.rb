fe3e92453a13683da7feb4dc81f8a953c0fd3e61
Tamiko Terada <tamikodevo@gmail.com>
2017-03-06 23:05:05 -0800

    method added: rider#initialize created and passed specs

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,15 +1,21 @@
 # new Rider, subclass of RideShare
-
-# create new trip and take in id, name, license, vin
-# @id
-# @name
-# @phone_number
+module RideShare
+  class Rider
+    attr_reader :id, :name, :phone_number
+    # create new rider and take in id, name, and phone
+    def initialize(rider_info) # ={}
+      @id = rider_info[:id]
+      @name = rider_info[:name]
+      @vin = rider_info[:phone_number]
+    end
+  end
+end
 
 # instance method: past_trips
 # return collection of trip instances by this rider
 
 # instance method: past_drivers
-# return rounded average rating across all trips by this rider
+# return collection of driver instances (through the trips functionality)
 
 # class method: all
 # read in CSV file for all instances of riders
