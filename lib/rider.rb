1c1304d16c9ee7532183f92a9ccbdd0422f7314b
add2point71dots <alcland@gmail.com>
2017-03-07 11:21:26 -0800

    Adds and tests Rider.all

--- lib/rider.rb
+++ lib/rider.rb
@@ -6,4 +6,16 @@ class RideShare::Rider
     @name = rider_hash[:name]
     @phone = rider_hash[:phone]
   end
+
+  def self.all
+    riders = []
+    CSV.read("support/riders.csv")[1..-1].each do |line|
+      riders << RideShare::Rider.new( {
+        id: line[0].to_i,
+        name: line[1],
+        phone: line[2]
+      } )
+    end
+    return riders
+  end
 end
--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -2,6 +2,8 @@ require_relative 'spec_helper'
 
 describe "Rider tests" do
   let(:rider) { RideShare::Rider.new({ id: 8, name: "Galois", phone: "206-555-2468" }) }
+  let(:riders_array) { RideShare::Rider.all }
+  let(:csv_info) { CSV.read('support/riders.csv') }
 
   describe "Rider#initialize" do
     it "Takes an ID, name, and phone number" do
@@ -16,4 +18,29 @@ describe "Rider tests" do
     end
   end
 
+  describe "Rider.all" do
+    it "Rider.all returns an array" do
+      riders_array.must_be_instance_of Array
+    end
+
+    it "Everything in the array is a Rider" do
+      riders_array.each do |rider|
+        rider.must_be_instance_of RideShare::Rider
+      end
+    end
+
+    it "The number of riders is correct" do
+      riders_array.length.must_equal csv_info.count - 1
+    end
+
+    it "The information for the first & last rider is correct" do
+      riders_array[0].id.must_equal csv_info[1][0].to_i
+      riders_array[0].name.must_equal csv_info[1][1]
+      riders_array[0].phone.must_equal csv_info[1][2]
+
+      riders_array[-1].id.must_equal csv_info[-1][0].to_i
+      riders_array[-1].name.must_equal csv_info[-1][1]
+      riders_array[-1].phone.must_equal csv_info[-1][2]
+    end
+  end
 end
