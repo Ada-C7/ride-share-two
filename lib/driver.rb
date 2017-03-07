43b9a651abc5a9e1c506fadc0fbf7426b76b8517
jjousun <jjousun@gmail.com>
2017-03-06 22:02:21 -0800

    Adding specs and code to driver

--- lib/driver.rb
+++ lib/driver.rb
@@ -9,15 +9,31 @@ class Driver
     @vin = vin
   end
 
-  # def get_driver_info
-  #   @info = []
-  #
-  #   data = CSV.open("../support/drivers.csv")
-  #
-  #   data.each do |line|
-  #     @info << line
-  #   end
-  # end
+  def self.all
+    driver_info = []
 
+    all_driver = CSV.open("/Users/jou-jousun/ada/projects/ride-share-two/support/drivers.csv")
+
+    all_driver.each do |driver|
+      driver_info << Driver.new(driver[0].to_i, driver[1].to_s, driver[2].to_s)
+    end
+
+    return driver_info
+
+    # get info from CSV
+    # create new instances of Driver
+    # push each new Driver instances into an driver_info
+    # return the array
+  end
+
+  #.find(id) when you input id, it will return the driver (instance) with that id
+  def self.find(id) #could use Class variable here instead of loading Accounts array each time
+    Driver.all.each do |driver|
+      if driver.id == id
+        return driver
+      end
+    end
+    raise ArgumentError.new "Driver doesn't exist"
+  end
 
 end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -10,20 +10,55 @@ describe "Driver" do
   end
 
   describe "trips" do
-    it "text" do
+    it "returns the list of trip instances that only this driver has taken" do
+      skip
       kind_driver = Driver.new(1, "Santa Claus", 98109)
       kind_driver.trips.must_be_instance_of Array
       # each trip (element) must be an Array, as trips is an array of arrays
     end
   end
 
-  describe "all" do
+  describe "Driver.all" do
+    before do
+      @drivers = Driver.all
+    end
 
+    it "returns all drivers from the CSV file" do
+      @drivers.must_be_instance_of Array
+
+      (@drivers.each { |i| return i }).must_be_instance_of Driver
+
+      @drivers.length.must_equal 100
+    end
   end
 
-  describe "find" do
+  describe "Driver.find" do
+    before do
+      @drivers = Driver.all
+    end
 
+    it "returns a driver that exists" do
+      driver = Driver.find(@drivers[0].id)
+      driver.must_be_instance_of Driver
+    end
+
+    it "can find the first driver from the CSV" do
+      driver = Driver.find(0)
+      driver.id.must_equal @drivers[0].id
+    end
+
+    it "can find the last driver from the CSV" do
+      driver = Driver.find(100)
+      driver.id.must_equal @drivers[-1].id
+    end
+
+    it "Raises an error for a driver that doesn't exist" do
+        proc {
+          Driver.find(111111)
+        }.must_raise ArgumentError
+    end
   end
+
 
   describe "rating" do
 
