3b1544284a748e1a88e03a7c87e2be1c75812ce1
sai samant <saisamant@gmail.com>
2017-03-06 16:32:13 -0800

    added pseudocode for trip.rb

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,3 +1,33 @@
-class Trips
+class Trip
+
+  def initialize trip_id, driver_id, rider_id, date, rating
+    @id = trip_id
+    @driver_id = driver_id
+    @rider_id = rider_id
+    @date = date
+    @rating = rating
+  end
+
+  def self.all
+    #reads trips.csv and puts info into array of Trip instances
+  end
+
+  def self.find_trips_by_rider
+    #call all
+    #search by rider_id to return array of Trip instances associated with that rider
+  end
+
+  def self.find_trips_by_driver
+    #call all
+    #search by driver to return array of Trip instances associated with that driver
+  end
+
+  def driver
+    #call Driver.find to return Driver instance associated with the driver_id of the trip instance
+  end
+
+  def rider
+    #call Rider.find to return Rider instance associated with the rider_id of the trip instance
+  end
 
 end
