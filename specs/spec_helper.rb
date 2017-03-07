0dbc6b0a16faababf523f67257be69e99110e8e2
AlisonZ <alisonkzerbe@gmail.com>
2017-03-06 16:38:10 -0800

    sets up file structure for project

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,8 +1,14 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+# didn't include these in Scrabble and it still worked, why?
+require_relative '../lib/driver.rb'
+require_relative '../lib/rider.rb'
+require_relative '../lib/trips.rb'
