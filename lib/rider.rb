c9550405e311fa0aa113b2528847b1f99ca514ca
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-07 11:02:22 -0800

    adds rider class methods and specs

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,11 +1,30 @@
-# HAS
-# @id, @name, @phone_number
-# ID, name and phone number
-#
 # INSTANCE METHODS
 # trips: retrieve the list of trip instances that only this rider has taken
 # drivers: retrieve the list of all previous driver instances (through the trips functionality built above)
-#
-# CLASS METHODS
-# all: retrieve all riders from the CSV file
-# find: find a specific rider using their numeric ID
+
+module RideShare
+  class Rider
+    attr_reader :id, :name
+
+    def initialize(params)
+      @id = params[:id].to_i
+      @name = params[:name]
+      @phone_number = params[:phone_number]
+    end
+
+    def self.all
+      @@all ||= CSV.read("support/riders.csv", headers:true).map do |line|
+        Rider.new(
+          id: line[0].to_i,
+          name: line[1],
+          phone_number: line[2]
+        )
+      end
+    end
+
+    def self.find(target_id)
+      all.find { |rider| rider.id == target_id }
+    end
+
+  end
+end
--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -1,2 +1,42 @@
 require_relative 'spec_helper'
-require_relative '../lib/driver'
+require_relative '../lib/rider'
+
+describe RideShare::Rider do
+  describe "#initialize" do
+    it "Doesn't create a rider with missing parameters" do
+      proc {
+        RideShare::Rider.new
+      }.must_raise ArgumentError
+    end
+  end
+
+  describe "self.all" do
+    let(:all) { RideShare::Rider.all }
+
+    it "Creates an array of all rider from the csv file" do
+      all.must_be_instance_of Array
+      all.first.must_be_instance_of RideShare::Rider
+    end
+
+    it "Includes the first rider in the csv file" do
+      all.first.name.must_equal "Nina Hintz Sr."
+    end
+
+    it "Includes the last rider in the csv file" do
+      all.last.name.must_equal "Miss Isom Gleason"
+    end
+  end
+
+  describe "self.find" do
+    it "Returns a rider object when given a valid rider ID" do
+      rider = RideShare::Rider.find(139)
+
+      rider.must_be_instance_of RideShare::Rider
+      rider.name.must_equal "Adah Miller"
+    end
+
+    it "Returns nil when given an invalid rider ID" do
+      RideShare::Rider.find("TRICKERY!").must_be_nil
+    end
+  end
+end
