8f920859d5eaed9c4798eda4dda0b67ac12c57d2
jjousun <jjousun@gmail.com>
2017-03-06 16:38:22 -0800

    Added spec_helper stuff

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,8 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
+require 'minitest/skip_dsl'
+
+require_relative '../lib/ride_share_2'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-
-# Require_relative your lib files here!
