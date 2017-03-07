b8f6215128feab34d7fce6a949be8680dbdee871
Jackie Watanabe <sushiigirl@gmail.com>
2017-03-07 10:31:35 -0800

    created basic structure of files and spec files

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,8 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
 # Add simplecov
 
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
