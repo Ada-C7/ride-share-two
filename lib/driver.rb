1769118e4a86dac47d844d1e47ade41eb6141ef7
Anacelia Gillette <acgillette426@gmail.com>
2017-03-06 16:38:30 -0800

    Starting on self.method tests

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,4 +1,5 @@
 require_relative 'trip'
+require 'csv'
 
 module RideShare
 
@@ -6,9 +7,24 @@ module RideShare
     attr_reader :id, :name, :vin
 
     def self.all
+      driver_array = []
+      CSV.open("support/drivers.csv", 'r').each do |driver|
+        new_driver = Driver.new({
+          :id => driver[0],
+          :name => driver[1],
+          :vin => driver[2]
+          })
+          driver_array << new_driver
+      end
+      driver_array.shift
+      return driver_array
     end
 
-    def self.find
+    def self.find(id)
+      drivers = Driver.all
+      drivers.each do |driver|
+        return driver if driver.id == id
+      end
     end
 
     def initialize(driver)
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -19,6 +19,52 @@ describe "Driver Class" do
       driver.must_respond_to :name
       driver.must_respond_to :vin
     end
+  end
+
+  describe "self.all" do
+    it "Returns an array" do
+      drivers = RideShare::Driver.all
+      drivers.must_be_instance_of Array
+    end
+
+    it "Returns an Array with elements" do
+      drivers = RideShare::Driver.all
+      drivers.length.must_be :>, 0
+    end
+
+    it "Returns an array of Driver Objects" do
+      drivers = RideShare::Driver.all
+      drivers.each do |driver|
+        driver.must_be_instance_of RideShare::Driver
+      end
+    end
+
+    it "Each driver object contains an id, name and vin" do
+      drivers = RideShare::Driver.all
+      drivers.each do |driver|
+        driver.must_respond_to :id
+        driver.must_respond_to :name
+        driver.must_respond_to :vin
+      end
+    end
+
+    it "Contains the correct information" do
+      drivers = RideShare::Driver.all
+      first = drivers[0]
+      first.name.must_equal "Bernardo Prosacco"
+      first.id.must_equal "1"
+      first.vin.must_equal "WBWSS52P9NEYLVDE9"
+    end
+  end
+
+  describe "self.find" do
+    it "Returns a driver object based on ID" do
+      driver = RideShare::Driver.find("1")
+      driver.must_be_instance_of RideShare::Driver
+    end
+
+    it "Returns the correct driver object" do
+    end
 
   end
 
