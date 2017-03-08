c606bfe5d0a4262a4c3f3276939c2688569ed6a2
Ashtn <ashtonehorton@gmail.com>
2017-03-07 16:35:25 -0800

    specs for self.all passed in driver spec, first stub for trips taken spec written

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,34 +1,64 @@
-class Driver
-# @@drivers = {} #or []
+module RideShare
+  class Driver
 
-#initialize
-# @id = driver_hash[id:]
-# @name = driver_hash[name:]
-# @vin = driver_hash[vin:]
-  #vin number should be a specific length to ensure it is a valid vehicle identification number <-method?
-#end
+    @@drivers = nil
 
-#def trips_taken
-#return list of trips taken by an instance of a diver
-#end
+    attr_reader :driver_id, :name, :vin
 
-
-#def average_rating
-#average_rating = ratings.all / ratings.length
-#return average_rating
-#end
-
-#self.all
-#reads csv file
-#creates new dirvers instances
-#returns array/hash of all drivers
-#@@drivers <<
-#end
-
-#self.find(id)
-#find id in @@drivers
-#returns a single driver associated with that id
-#end
+    def initialize(driver_hash)
+      @driver_id = driver_hash[:driver_id]
+      @name = driver_hash[:name]
+      @vin = driver_hash[:vin]
+      # vin number should be a specific length to ensure it is a valid vehicle identification number <-method?
+    end
 
 
-end
+    def trips_taken
+      trips = RideShare::Trip.find(@driver_id)
+      return trips
+    end
+
+
+  #def average_rating
+  #average_rating = ratings.all / ratings.length
+  #return average_rating
+  #end
+
+  #self.find(id)
+  #find id in @@drivers
+  #returns a single driver associated with that id
+  #end
+
+
+      # self.all
+      # # reads csv file
+      #
+      # # creates new dirvers instances
+      # # returns array/hash of all drivers
+      # # @@drivers <<
+      # end
+
+
+
+    def self.all
+
+      if @@drivers == nil
+
+        @@drivers = []
+
+        CSV.read("support/drivers.csv").each do |row|
+          driver = {
+            driver_id: row[0].to_i,
+            name: row[1],
+            vin: row[2]
+          }
+
+          @@drivers << RideShare::Driver.new(driver)
+
+        end
+      end
+      return @@drivers
+    end #
+
+  end #end of class
+end #end of module
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -2,52 +2,62 @@ require_relative 'spec_helper'
 
 describe "Driver Class" do
 
-  let(:ada) { Driver.new(name: 'Ada', id: 2, vin: 12345338303493234 )}
+  let(:ada) { RideShare::Driver.new(name: 'Ada', driver_id: 2, vin: 12345338303493234 )}
 
   describe "initialize method" do
 
     it "Instantiates a new instance of Driver class" do
-      ada.must_be_instance_of Driver
+      ada.must_be_instance_of RideShare::Driver
     end
 
     it "Stored data must match what was passed as an argument" do
       ada.name.must_equal "Ada"
-      ada.id.must_equal 2
+      ada.driver_id.must_equal 2
       ada.vin.must_equal 12345338303493234
     end
   end #end of initialze method
 
-  describe "self.all method" do
+  xdescribe "trips_taken" do
 
-    let(:drivers) {Driver::all}
-
-    it "Driver.all returns array" do
-      drivers.class.must_equal Array
+    it "Trips taken must be an array" do
+      ada.trips_taken.must_be_instance_of Array
     end
 
-    it "Each driver info is stored in a hash" do
-      skip
-      drivers.first.must_be_instance_of Hash
-      drivers.last.must_be_instance_of Hash
+  end #end of trips taken
+
+  describe "self.all method" do
+
+    let(:my_drivers) {RideShare::Driver::all}
+
+    it "Driver.all returns array" do
+
+      my_drivers.class.must_equal Array
+    end
+
+    it "First and Last drivers are instansces of Driver Class" do
+
+      my_drivers.first.must_be_instance_of RideShare::Driver
+      my_drivers.last.must_be_instance_of RideShare::Driver
     end
 
     it "The number of drivers is correct" do
-      skip
-      drivers.length.must_equal CSV.read(support/drivers.csv).count
+      my_drivers.length.must_equal CSV.read("support/drivers.csv").count
     end
 
     it "The Id, name and vin number matches the first account " do
-      skip
-      drivers.first.id.must_equal
-      drivers.first.name.must_equal
-      drivers.first.vin.must_equal
+
+      index = 1
+
+      my_drivers[index].driver_id.must_equal 1
+      my_drivers[index].name.must_equal "Bernardo Prosacco"
+      my_drivers[index].vin.must_equal "WBWSS52P9NEYLVDE9"
     end
 
-    it "the Id, name and vin number matched the last account" do
-      skip
-      drivers.last.id.must_equal
-      drivers.last.name.must_equal
-      drivers.last.vin.must_equal
+    it "The Id, name and vin number matched the last account" do
+    
+      my_drivers.last.driver_id.must_equal 100
+      my_drivers.last.name.must_equal "Minnie Dach"
+      my_drivers.last.vin.must_equal "XF9Z0ST7X18WD41HT"
     end
 
   end #end of self.all
