7a5e266ad5c6de22ee56b9839b825e77060ea7f8
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-08 13:08:37 -0800

    finish testing for Rider.trips

--- specs/riders_spec.rb
+++ specs/riders_spec.rb
@@ -2,6 +2,7 @@ require_relative 'spec_helper'
 
 describe "rider_id" do
   let(:my_rider) {RideShare::Rider.new(1, 2, 3)}
+  let(:trips_csv) {CSV.read("support/trips.csv")}
 
   describe "Diver#initialize" do
     it "takes an ID, Name, and Phone Number to initialize" do
@@ -16,27 +17,27 @@ describe "rider_id" do
       my_rider.trips.must_be_instance_of Array
     end
 
-  #   it "each item is of class Trip" do
-  #     my_driver.trips.each do |trip|
-  #       trip.must_be_instance_of RideShare::Trip
-  #     end
-  #   end
-  #
-  #   it "trips.length matches number of trips from CSV file for that driver" do
-  #     trips_number = my_driver.trips.length
-  #     lines_from_csv = []
-  #     trips_csv.each do |line|
-  #       if line[1].to_i == my_driver.id
-  #         lines_from_csv << line
-  #       end
-  #     end
-  #     lines_from_csv.length.must_equal trips_number
-  #   end
-  #
-  #   it "Returns 0 if driver is not found" do
-  #     bad_id = RideShare::Driver.new(77777, "Mr. Shanie Gusikowski", "XF9HHMKS402GD41NF")
-  #     bad_id.trips.must_equal 0
-  #   end
+    it "each item is of class Trip" do
+      my_rider.trips.each do |trip|
+        trip.must_be_instance_of RideShare::Trip
+      end
+    end
+
+    it "trips.length matches number of trips from CSV file for that rider" do
+      trips_number = my_rider.trips.length
+      lines_from_csv = []
+      trips_csv.each do |line|
+        if line[2].to_i == my_rider.id
+          lines_from_csv << line
+        end
+      end
+      lines_from_csv.length.must_equal trips_number
+    end
+
+    it "Returns 0 if driver is not found" do
+      bad_id = RideShare::Rider.new(901291029102, "Nope", "Nope")
+      bad_id.trips.must_equal 0
+    end
   end
 
 end
