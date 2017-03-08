511ce88d6df3fd9578a4018498230805f43cf13e
tehut <tehut.m.g@gmail.com>
2017-03-07 18:35:13 -0800

    wrote specs for trip.rb

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,26 +1,51 @@
 require_relative 'spec_helper.rb'
 
-describe 'Trip class properties' do
-  let(:my_trip) {Rideshare::Trip.new}
+describe 'Trip Class Properties' do
+  let(:my_trip){Rideshare::Trip.new({trip_id:"5", driver_id:"32", rider_id:"34",date:"05-29-2017", rating:"5"})}
 #tests that driver class initalizes
-  it 'will find driver class' do
+  it 'will find trip class' do
     my_trip.class.must_equal Rideshare::Trip
   end
   #tests that the trip id instance variable is exposed
+  it "user can call Trip.trip_id" do
+    my_trip.trip_id.must_equal "5"
+  end
   #tests that the rider id instance variable is exposed
+  it "user can call Trip.rider_id" do
+    my_trip.rider_id.must_equal "34"
+  end
   #tests that the driver id instance variable is exposed
-
-  #tests that the trip instance variable is exposed
+  it "user can call Trip.driver_id" do
+    my_trip.driver_id.must_equal "32"
+  end
+  #tests that the rating instance variable is exposed
+  it "user can call Trip.rating" do
+    my_trip.rating.must_equal "5"
+  end
 end
 
-describe "Tests class methods for Driver " do
+describe "Class Methods for Trip " do
    #tests that you can generate all trip instances from csv
-   #tests that you can generate a specific trip for a given driver id
-   #tests that you can generate a specific trip for a given rider id
-end
+   it "can generate all trip instances from the csv file" do
+     Rideshare::Trip.create_trips["1"].class.must_equal Rideshare::Trip
+     Rideshare::Trip.create_trips.length.must_equal 600
+   end
+   #tests that you can generate all of a given driver's trips
+   it "can generate all trips for a specific driver_id" do
+     array = Rideshare::Trip.find_driver(1).keys
+     Rideshare::Trip.find_driver(1)[array[0]].class.must_equal Rideshare::Trip
 
-describe "Tests instance methods for Driver " do
-  let(:my_driver) {Rideshare::Driver.new}
-   #tests that you can call the associated driver from a trip object
-   #tests that you can call the associated driver from a trip object
+   end
+   #tests that you can generate all of a given rider's trips
+   it "can generate all trips for a specific driver_id" do
+     array = Rideshare::Trip.find_rider(1).keys
+     Rideshare::Trip.find_rider(1)[array[0]].class.must_equal Rideshare::Trip
+
+   end
  end
+#
+# describe "Tests instance methods for Driver " do
+#   let(:my_driver) {Rideshare::Driver.new}
+#    #tests that you can call the associated driver from a trip object
+#    #tests that you can call the associated driver from a trip object
+#  end
