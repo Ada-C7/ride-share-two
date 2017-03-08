c1be632f7d92654c8895e7cd66f77b05ec90b5af
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-08 13:04:31 -0800

    added initial testing for Rider.trips and method

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,14 +1,17 @@
 
 class RideShare::Rider
   attr_reader :id, :name, :phone
-  
+
   def initialize(id, name, phone)
     @id = id
     @name = name
     @phone = phone
   end
-  # Each rider should be instantiated (INITIALIZE) with an ID, name and phone number
-  #   input: Rider.new
-  #   output: new rider object.
 
+  def trips
+    RideShare::Trip.find_all_rider(@id)
+  end
+  # Retrieve a list of trips only this rider has taken
+  #   input: Rider ID
+  #   output: Array of Trip instances
 end
--- specs/riders_spec.rb
+++ specs/riders_spec.rb
@@ -6,7 +6,37 @@ describe "rider_id" do
   describe "Diver#initialize" do
     it "takes an ID, Name, and Phone Number to initialize" do
       my_rider.must_respond_to :id
+      my_rider.must_respond_to :name
+      my_rider.must_respond_to :phone
     end
+  end
+
+  describe "Rider#trips" do
+    it "returns an array of trips that this rider has taken" do
+      my_rider.trips.must_be_instance_of Array
+    end
+
+  #   it "each item is of class Trip" do
+  #     my_driver.trips.each do |trip|
+  #       trip.must_be_instance_of RideShare::Trip
+  #     end
+  #   end
+  #
+  #   it "trips.length matches number of trips from CSV file for that driver" do
+  #     trips_number = my_driver.trips.length
+  #     lines_from_csv = []
+  #     trips_csv.each do |line|
+  #       if line[1].to_i == my_driver.id
+  #         lines_from_csv << line
+  #       end
+  #     end
+  #     lines_from_csv.length.must_equal trips_number
+  #   end
+  #
+  #   it "Returns 0 if driver is not found" do
+  #     bad_id = RideShare::Driver.new(77777, "Mr. Shanie Gusikowski", "XF9HHMKS402GD41NF")
+  #     bad_id.trips.must_equal 0
+  #   end
   end
 
 end
