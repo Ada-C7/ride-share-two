1fa94e1c6643c70549c3311b5c63f89b79a0eecc
tehut <tehut.m.g@gmail.com>
2017-03-08 15:55:47 -0800

    modified create trips array to reject any trips with ratings outside the range 1-5

--- lib/trip.rb
+++ lib/trip.rb
@@ -20,27 +20,29 @@ module Rideshare
       CSV.foreach('support/trips.csv', {:headers => true}) do |row|
        array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]})
      end
+        array = array.reject{|value|value.rating.to_i < 1 || value.rating.to_i > 5}
      return array
     end
 
 
     def self.find_by_driver(param)
-      array = Trip.create_trips.select{| value| value[1]== param.to_s}
-      # array = []
-      # CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
-      # array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[1] == param.to_s
-      # end
-      return array
+      return Trip.create_trips.select{|value| value.driver_id== param.to_s}
+
     end
 
     def self.find_by_rider(param)
-      array = Trip.create_trips.select{| value| value[2]== param.to_s}
+      array = Trip.create_trips.select{| value| value.rider_id== param.to_s}
+
+      value = "That Rider has not used the service" if array.length < 0
+      value = array if array.length > 0
       # array =[]
       # CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
       # array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[2] == param.to_s
       # end
       return array
     end
+
+
 
     def make_driver
     #helper method to extract driver_id from trip object
@@ -55,8 +57,11 @@ module Rideshare
 end
 
 
+
 puts Rideshare::Trip.create_trips[0].make_rider
 puts Rideshare::Trip.create_trips[0].make_driver
+puts Rideshare::Trip.find_by_driver(17)
+puts Rideshare::Trip.find_by_rider(300)
 
 #
 #
