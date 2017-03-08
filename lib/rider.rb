8df6c8e1eee751b618da7b9aabdad8bbcd052127
amb54 <ambarklund@hotmail.com>
2017-03-08 13:51:58 -0800

    Rider#self.all tested and passed

--- lib/rider.rb
+++ lib/rider.rb
@@ -9,5 +9,13 @@ module RideSharing
       @phone_num = phone_num
     end
 
+    def self.all(path = "./support/riders.csv")
+      all_riders =[]
+      CSV.foreach(path, {:headers => true}) do |line_array|
+        all_riders << self.new(line_array[0].to_i, line_array[1], line_array[2])
+      end
+      return all_riders
+    end
+
   end # End of class Rider
 end # End of module RideSharing
--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -18,5 +18,29 @@ describe "Rider" do
       rider1.must_respond_to :phone_num
       rider1.phone_num.must_equal phone_num
     end
+
+    describe "#self.all" do
+      it "Returns an array." do
+        RideSharing::Rider.all.must_be_kind_of Array
+      end
+
+      it "The elements in the returned array must be RideSharing::Rider instances " do
+        all_riders = RideSharing::Rider.all
+        all_riders.each do |rider_obj|
+          rider_obj.must_be_kind_of RideSharing::Rider
+        end
+        all_riders.length.must_equal 300
+      end
+
+      it "The phone number of the second instance must be (392) 217-0777" do
+        RideSharing::Rider.all[1].phone_num.must_equal "(392) 217-0777"
+      end
+
+      it "The name of rider with id number 252 must be Cameron Casper IV" do
+        rider1 = RideSharing::Rider.all.select {|rider_obj| rider_obj.id == 252}.first
+        rider1.name.must_equal "Cameron Casper IV"
+      end
+    end # End of describe "Driver#self.all"
+
   end # End of describe "#initialize"
 end # End of describe "Rider"
