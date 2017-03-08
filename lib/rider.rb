b155cdf173a9a53148af8ee0e5723b57100725dd
Ashtn <ashtonehorton@gmail.com>
2017-03-08 11:26:13 -0800

    removed puesdocode

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,39 +1,3 @@
-class Rider
-
-#@@riders = {} #or []
-
-#def initiatize
-# @id = rider_hash[:rider_id]
-# @name = rider_hash[name:]
-# @phone_number = rider_hash[phone_number:]
-#end
-
-#def trips_taken
-#rider_id = @rider_id
-#maybe use Trip::find_rider(rider_id)
-# return list of trips taken by this rider
-#end
-
-#def previous_drivers
-#retrieve the list of all previous rider instances from trips_take method
-#return list of all previous drivers
-#end
-
-#def self.all
-#reads csv
-#creates rider instances
-#returns all riders
-#end
-
-#def self.find(rider_id)
-#checks @@riders
-#returns rider data with associating id
-#end
-
-end
-
-
-
 module Carmmunity
   class Rider
 
