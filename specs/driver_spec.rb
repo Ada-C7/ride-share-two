9c0d115db11de5a6a3d8a487453fc61533c96d13
Kayla <kubicke.kayla@gmail.com>
2017-03-08 13:55:05 -0800

    Updated self.all tests.

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -1,3 +1,5 @@
+# TODO: Make sure array is picking values correctly.
+
 require_relative 'spec_helper.rb'
 
 describe "retrieve an array from self.all" do
@@ -5,7 +7,26 @@ describe "retrieve an array from self.all" do
     # TODO: update to let()
     @driver = RideShare::Driver.all
   end
-  it "retrieve an array" do
+  it "retrieves an array" do
     @driver.must_be_instance_of Array
   end
+
+  it "retrieves an id, name, and vin number" do
+    CSV.read('support/drivers.csv') do |line|
+      counter = 0
+      @driver[counter][0].driver_id.must_equal line[counter][0].to_i #equal to? @driver[counter].driver_id.must_equal line[counter][0].to_i
+      @driver[counter][1].name.must_equal line[counter][1].to_s #.name
+      @driver[counter][2].vin.must_equal vin[counter][2].to_s #.vin
+    end
+  end
+
+  it "verifies vin length" do
+    CSV.read('support/drivers.csv') do |line|
+      counter = 0
+      @driver[counter][2].length.must_equal 17
+      counter += 1
+    end
+  end
+
+  # TODO: Edgecase and 'middle' case test
 end
--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -1,3 +1,5 @@
+# TODO: Make sure array is picking values correctly.
+
 require_relative 'spec_helper.rb'
 
 
@@ -8,5 +10,16 @@ describe "retrieve an array from self.all" do
   end
   it "retrieve an array" do
     @rider.must_be_instance_of Array
+  end
+
+  it "retrieves the rider_id, name, phone_num, and new_rider" do
+    CSV.read('support/riders.csv') do |line|
+      counter = 0
+
+      @rider[counter][0].rider_id.must_equal line[counter][0].to_i
+      @rider[counter][1].name.must_equal line[counter][1].to_i
+      @rider[counter][2].phone_num.must_equal line[counter][2].to_i
+      @rider[counter][3].new_rider.must_equal line[counter][3].to_s
+    end
   end
 end
