08195e7f44eee6532ca8f1ed5805f78afb19e25a
Haby Randall <sow.haby@gmail.com>
2017-03-08 00:14:58 -0800

    Added specs and self methods to Trip.rb

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 module RideShare
 
   class Trip
@@ -9,24 +11,41 @@ module RideShare
       @rider_id = rider_id
       @date = date
       @rating = rating
-      raise ArgumentError.new("Rating must be between 1 and 5") if rating.length < 1 || rating.length > 5
+      # raise ArgumentError.new("Rating must be between 1 and 5") if @rating.length < 1 || @rating.length > 5
     end
 
 
     def self.all
-      
+      trip_array = []
+      CSV.open('support/trips.csv', 'r', :headers => true).each do |line|
+        trip_id = line["trip_id"].to_i
+        driver_id = line["driver_id"].to_i
+        rider_id =  line["rider_id"].to_i
+        date = line["date"].to_s
+        rating = line["rating"].to_i
+        trip_array << self.new(trip_id, driver_id, rider_id, date, rating)
+      end
+      return trip_array
     end
-    #Each trip has ID, rider_id, driver_id, date, rating
-    #Rating must be between 1-5
-    #Retrieve driver instance through driver_id
-    #retrieve driver instance through rider_id
 
-    #Reads CSV file in all method
-    #create method that uses method from driver to retrieve the associated driver instance
-    #create method that uses method from rider to retrieve the associated rider instance
-
-    #create a method to retrieve all trips by driver_id
-    #create a method to retrieve all trips by rider_id
+    def self.find(trip_id)
+      Trip.all.each do |file|
+        if file.trip_id == trip_id
+          return file
+        end
+      end
+      raise ArgumentError.new("This account does not exist")
+    end
   end
+  #Each trip has ID, rider_id, driver_id, date, rating
+  #Rating must be between 1-5
+  #Retrieve driver instance through driver_id
+  #retrieve driver instance through rider_id
 
+  #Reads CSV file in all method
+  #create method that uses method from driver to retrieve the associated driver instance
+  #create method that uses method from rider to retrieve the associated rider instance
+
+  #create a method to retrieve all trips by driver_id
+  #create a method to retrieve all trips by rider_id
 end
--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -3,14 +3,14 @@ require_relative '../specs/spec_helper.rb'
 describe "Trip" do
 
   describe "Trip#initialize" do
-    let(:my_trip) {RideShare::Trip(1, 1, 54, "2016-04-05", 3)}
+    let(:my_trip) {RideShare::Trip.new(1, 1, 54, "2016-04-05", 3)}
 
     it "Should create a new instance of trip" do
       my_trip.must_be_instance_of RideShare::Trip
     end
 
-    it "Should be associated with :id, :driver_id, :rider_id, :date, and :rating" do
-      my_trip.must_respond_to :id
+    it "Should be associated with :trip_id, :driver_id, :rider_id, :date, and :rating" do
+      my_trip.must_respond_to(:trip_id)
       my_trip.must_respond_to :driver_id
       my_trip.must_respond_to :rider_id
       my_trip.must_respond_to :date
@@ -26,27 +26,27 @@ describe "Trip" do
 
   describe "Trip#find" do
     it "Should return the first account from the CSV file" do
-      RideShare::Trip.find(1).id.must_equal(1)
+      RideShare::Trip.find(1).trip_id.must_equal(1)
       RideShare::Trip.find(1).driver_id.must_equal(1)
       RideShare::Trip.find(1).rider_id.must_equal(54)
       RideShare::Trip.find(1).date.must_equal("2016-04-05")
       RideShare::Trip.find(1).rating.must_equal(3)
     end
-
+# 30,28,230,2016-10-12,5
     it "Should return an existing account from the CSV file" do
-      RideShare::Trip.find(30).id.must_equal()
-      RideShare::Trip.find(30).driver_id.must_equal()
-      RideShare::Trip.find(30).rider_id.must_equal()
-      RideShare::Trip.find(30).date.must_equal()
-      RideShare::Trip.find(30).rating.must_equal()
+      RideShare::Trip.find(30).trip_id.must_equal(30)
+      RideShare::Trip.find(30).driver_id.must_equal(28)
+      RideShare::Trip.find(30).rider_id.must_equal(230)
+      RideShare::Trip.find(30).date.must_equal("2016-10-12")
+      RideShare::Trip.find(30).rating.must_equal(5)
     end
 
     it "Should return the last account from the CSV file" do
-      RideShare::Trip.find(600).id.must_equal()
-      RideShare::Trip.find(600).driver_id.must_equal()
-      RideShare::Trip.find(600).rider_id.must_equal()
-      RideShare::Trip.find(600).date.must_equal()
-      RideShare::Trip.find(600).rating.must_equal()
+      RideShare::Trip.find(600).trip_id.must_equal(600)
+      RideShare::Trip.find(600).driver_id.must_equal(61)
+      RideShare::Trip.find(600).rider_id.must_equal(168)
+      RideShare::Trip.find(600).date.must_equal("2016-04-25")
+      RideShare::Trip.find(600).rating.must_equal(3)
     end
 
     it "Should Raise an error when the account does not exist" do
