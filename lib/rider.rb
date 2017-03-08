017c08292c585fceb193b7149b355616bd8d19e5
tehut <tehut.m.g@gmail.com>
2017-03-07 18:37:11 -0800

    adapted scratch.rb to make class methods for rider class

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,6 +1,32 @@
+  require 'csv'
 module Rideshare
   #
-  class Riders
+  class Rider
+    attr_reader :rider_id, :name, :phone_num
+
+    def initialize(trip_hash)
+
+      @rider_id = trip_hash[:rider_id]
+      @phone_num = trip_hash[:phone_num]
+      @name = trip_hash[:name]
+
+    end
+
+    def self.create_rider
+      hash = {}
+      CSV.foreach('support/riders.csv', {:headers => true, :header_converters=> :symbol}) do |row|
+        hash[row[0]] = Rider.new({rider_id:row[0], name:row[1], phone_num:row[2]})
+      end
+      return hash
+    end
+
+
+    def self.find_rider(param)
+
+      CSV.foreach('support/riders.csv', {:headers=> true, :header_converters => :symbol}) do |row|
+        return row[0] =  Rider.new({rider_id:row[0], name:row[1], phone_num:row[2]}) if row[0] == param.to_s
+      end
+    end
     # require '..support/riders.csv'
     # require '..support/trips.csv'
 
@@ -66,7 +92,8 @@ module Rideshare
     #     my_drivers.each do |id| csv.open(id)
     # end
     #
-  end
+    #end
+end
 end
 #
 
