1b45f1ceb41396ba9a9b4acbcafa3ffe55e0533e
Spatterjaaay <aja.pursova@gmail.com>
2017-03-07 16:18:22 -0800

    addred require csv

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -7,6 +7,7 @@ require 'minitest/reporters'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+require 'csv'
 require_relative '../lib/driver'
 require_relative '../lib/rider'
 require_relative '../lib/trip'
