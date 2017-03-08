5ebdb1322577603c4578bb5f31d6d13e2d8124bd
tehut <tehut.m.g@gmail.com>
2017-03-08 14:57:31 -0800

    spent the whole day in this file and scratch.rb debugging an error i made when creating and testing instance methods. finally got create_trips, make_rider and make_driver method working.

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,4 +1,8 @@
+require 'pry'
 require 'csv'
+require_relative 'driver'
+require_relative 'rider'
+
 module Rideshare
   class Trip
     attr_reader  :driver_id, :rider_id, :rating, :trip_id
@@ -9,61 +13,50 @@ module Rideshare
       @rider_id = trip_hash[:rider_id]
       @date = trip_hash[:date]
       @rating = trip_hash[:rating]
-
     end
 
     def self.create_trips
-      hash = {}
-      CSV.foreach('support/trips.csv', {:headers => true, :header_converters=> :symbol}) do |row|
-       hash[row[0]] = Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]})
+      array = []
+      CSV.foreach('support/trips.csv', {:headers => true}) do |row|
+       array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]})
      end
-     return hash
+     return array
     end
 
 
     def self.find_by_driver(param)
-      array = []
-      CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
-      array = Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[1] == param.to_s
-      end
+      array = Trip.create_trips.select{| value| value[1]== param.to_s}
+      # array = []
+      # CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
+      # array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[1] == param.to_s
+      # end
       return array
     end
 
-    def self.find_rider(param)
-      array =[]
-      CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
-      array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[2] == param.to_s
-      end
+    def self.find_by_rider(param)
+      array = Trip.create_trips.select{| value| value[2]== param.to_s}
+      # array =[]
+      # CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
+      # array << Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[2] == param.to_s
+      # end
       return array
     end
-    def find_rider_id(param)
-    #helper method to extract rider_id from trip object
-      Rideshare::Trip.find_rider(param)[0].rider_id
-    end
-    #
-    # def find_driver_id(param)
-    # #helper method to extract driver_id from trip object
-    #   Rideshare::Trip.find_driver(param)[0].driver_id
-    # end
-    # def find_rider(param)
-    #   Rideshare::Rider.find_rider(self.find_rider(param)[rider_id])
-    #   end
-    # end
 
-    # find_driver(param)
-    #    self.create_trips unless trips_created?
-    #   if @alltrips.each |array| do
-    #    array[1] == param
-    #    driver_id = csv.open driver_id
-    #    return driver_id
-    #   end
-    # end
+    def make_driver
+    #helper method to extract driver_id from trip object
+      Rideshare::Driver.find_driver(self.driver_id)
+    end
+
+    def make_rider
+    #helper method to extract driver_id from trip object
+      Rideshare::Rider.find_rider(self.rider_id)
+    end
   end
 end
-#
-driver = Rideshare::Trip.find_by_rider(147)[0]
-print driver
 
-#
+
+puts Rideshare::Trip.create_trips[0].make_rider
+puts Rideshare::Trip.create_trips[0].make_driver
+
 #
 #
--- scratch.rb
+++ scratch.rb
@@ -1,7 +1,8 @@
 require 'csv'
 
+module Rideshare
 class Trip
-  attr_reader :all_trips, :driver_id, :rider_id
+  attr_reader  :driver_id, :rider_id, :rating, :trip_id
 
   def initialize(trip_hash)
     @trip_id = trip_hash[:trip_id]
@@ -12,23 +13,28 @@ class Trip
 
   end
 
-  def self.create_all_trips
+  def self.create_trips
     hash = {}
     CSV.foreach('support/trips.csv', {:headers => true, :header_converters=> :symbol}) do |row|
-     hash[row[0]] = {trip_id:row[0], driver_id:row[1], rider_id:row[2],date:Date.parse(row[3]), rating:row[4]}
-    end
-    return hash
+     hash[row[0]] = Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]})
+   end
+   return hash
   end
 
 
   def self.find_driver(param)
+    hash = {}
     CSV.foreach('support/trips.csv', {:headers=> true, :header_converters => :symbol}) do |row|
-
-        return  Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:Date.parse(row[3]), rating:row[4]}) if row[1] == param
-
+    hash[row[0]] = Trip.new({trip_id:row[0], driver_id:row[1], rider_id:row[2],date:row[3], rating:row[4]}) if row[1] == param.to_s
     end
+    return hash
   end
 end
-print Trip.find_driver("23").rider_id
+end
+# print Trip.find_driver("23").rider_id
 
-# print Trip.rider_id
+# puts Trip.find_driver(80).keys.class
+array = Rideshare::Trip.find_driver(1).keys
+puts array
+Rideshare::Trip.find_driver(1)[array[0]]
+# Trip.create_trips.length.must_equal 600
