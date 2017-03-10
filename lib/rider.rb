5dd0c52281201a56dc3fba0828f681337220da2d
Olivia <ssecretsharer@gmail.com>
2017-03-10 12:17:28 -0800

    All methods for rider written

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,67 +1,61 @@
-# #Each rider should:
-#
-# have an ID, name and phone number
-
-# Given a rider object, you should be able to:
-#
-# retrieve the list of trip instances that only this rider has taken
-
-# retrieve the list of all previous driver instances (through the trips functionality built above) #DRIVERS(TRIPS)
-
-# You should be able to:
-#
-# retrieve all riders from the CSV file
-
-# FIND a specific rider using their numeric ID
-
+s
 require 'csv'
 require 'pry'
 
 class Rider
 
-attr_accessor :rider_id, :name, :phone
+  attr_accessor :rider_id, :name, :phone
 
-def initialize()
-  @rider_id = hash[:rider_id]
-  @name = hash[:name]
-  @phone = hash[:phone]
-end
-
-
-def self.all #read in from csv
-  riders = []
-  riders_master = CSV.read("support/riders.csv")
-  riders_master.delete_at(0)
-  riders_master.each do |line|
-    rider_hash = {}
-    rider_hash[:rider_id] = line[0].to_i
-    rider_hash[:name] = line[1]
-    rider_hash[:phone] = line[2]
-    riders << Rider.new(rider_hash)
+  def initialize()
+    @rider_id = hash[:rider_id]
+    @name = hash[:name]
+    @phone = hash[:phone]
   end
-  return riders
-end
 
 
-def riders_trips
-  #find previous driver instances for this rider instance
-end
+  def self.all #read in from csv
+    riders = []
+    riders_master = CSV.read("support/riders.csv")
+    riders_master.delete_at(0)
+    riders_master.each do |line|
+      rider_hash = {}
+      rider_hash[:rider_id] = line[0].to_i
+      rider_hash[:name] = line[1]
+      rider_hash[:phone] = line[2]
+      riders << Rider.new(rider_hash)
+    end
+    return riders
+  end
+
+  def self.find(rider_id)
+    all_riders = Rider.all
+    find_rider = nil
+    all_riders.each do |rider|
+      find_rider = rider if rider.rider_id == rider_id
+    end
+    raise ArgumentError.new("Riders don't match") if find_rider == nil
+    return find_rider
+    #remember above loop as searching to reset from nil
+    # FIND a specific rider using their numeric ID
+  end
 
 
-def rider_drivers(trips)
+  def rider_trips(rider_id)
+    rider_trips = []
+    rider_trips = Rider.trips_by_rider(@rider_id)
+    #find all previous trip instances for this rider instance
+  end
+
   # retrieve the list of all previous driver instances (through the trips functionality built above) #DRIVERS(TRIPS)
-end
+
+  def drivers_per_rider(rider_id)
+    rider_drivers = []
+    trips_by_rider
+    #^returns all trips for single rider instance "rider_trips"
+    rider_trips.each do |driver_id|
+      rider_drivers << driver_id
+    end
+  end
 
 
-def drivers_past(id)
-  # retrieve the list of all previous driver instances (through the trips functionality built above) #DRIVERS(TRIPS)
-end
-
-
-
-
-
-
-
-
-end
+end#class end
