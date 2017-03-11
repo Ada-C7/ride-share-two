65c862300b5b68e199bc9d5da726b48958f278e0
Olivia <ssecretsharer@gmail.com>
2017-03-11 13:10:52 -0800

    Just making sure I've commited the latest tiny changes before I commit all latest changes to specs.

--- lib/rider.rb
+++ lib/rider.rb
@@ -1,4 +1,4 @@
-s
+
 require 'csv'
 require 'pry'
 
@@ -6,14 +6,14 @@ class Rider
 
   attr_accessor :rider_id, :name, :phone
 
-  def initialize()
+  def initialize(hash)
     @rider_id = hash[:rider_id]
     @name = hash[:name]
     @phone = hash[:phone]
   end
 
 
-  def self.all #read in from csv
+  def self.all
     riders = []
     riders_master = CSV.read("support/riders.csv")
     riders_master.delete_at(0)
@@ -30,7 +30,7 @@ class Rider
   def self.find(rider_id)
     all_riders = Rider.all
     find_rider = nil
-    all_riders.each do |rider|
+    all_riders.map do |rider|
       find_rider = rider if rider.rider_id == rider_id
     end
     raise ArgumentError.new("Riders don't match") if find_rider == nil
@@ -56,6 +56,5 @@ class Rider
       rider_drivers << driver_id
     end
   end
-
 
 end#class end
