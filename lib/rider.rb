1eeb06201911e5b2e4e15e6239e46bd39c68b6ac
Andrea Valliere <amvalliere@gmail.com>
2017-03-12 03:07:44 -0700

    Wrote self.all method for Rider class. Passes test.

--- lib/rider.rb
+++ lib/rider.rb
@@ -9,5 +9,13 @@ module RideShare
       @phone_num = phone_num
     end
 
+    def self.all
+      riders = []
+      CSV.open("./support/riders.csv", "r").each do |line|
+        riders << self.new(line[0], line[1], line[2])
+      end
+      return riders
+    end
+
   end
 end
--- specs/rider_spec.rb
+++ specs/rider_spec.rb
@@ -7,7 +7,16 @@ describe "Rider class" do
 
   describe "Initialize method" do
     it "Each instance has an ID, name, and phone number" do
+      rider_id = "0000"
+      name = "Buddy"
+      phone_num = "555-5555"
+      rider = RideShare::Rider.new(rider_id, name, phone_num)
 
+      rider.must_respond_to :rider_id
+      rider.rider_id.must_equal rider_id
+
+      rider.must_respond_to :name
+      rider.name.must_equal name
     end
   end
 
@@ -23,13 +32,15 @@ describe "Rider class" do
 
   describe "self.all method" do
     it "Retrieves list of all riders from CSV file" do
-
+      all_riders = RideShare::Rider.all
+      all_riders.must_be_instance_of Array, "This is not an array"
+      all_riders.length.must_equal 301
     end
   end
 
   describe "self.find method" do
     it "finds a specific rider given their rider ID" do
-      
+
     end
   end
 end
