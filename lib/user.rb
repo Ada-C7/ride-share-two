fc41c7e32a753ba602fc8f95fa0218097f225cda
Leesyruiz <leesyruiz@gmail.com>
2017-03-13 10:05:39 -0700

    final commit

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,38 @@
+# #this parenst class doesnt actually make sense... whoops sorry :D
+#
 # require 'csv'
+# require_relative
+#
 #
 # module RideShare
 #
 #   class User
 #
-#     attr_accessor :id, :name
+#     attr_reader :id,d :name
 #
-#     def initilize(args=nil)
-#       @id = id
-#       @name = name
+#     def initialize(args)
+#       @id =  args[:id]
+#       @name = args[:name]
 #     end
 #
-#   end # class
-# end #module
+#
+#
+#     def self.find(id)
+#       users = RideShare::Rider.all
+#       riders.each do |rider|
+#         return rider if rider.id == id
+#       end
+#     end
+#
+#
+#     def self.find(id)
+#       users = RideShare.all
+#       drivers.each do |driver|
+#         return driver if driver.id == id
+#       end
+#     end
+#
+#
+#
+#   end
+# end
