4ec9a16d6d1b16b9a3027c22f687c00735111510
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-12 19:39:40 -0700

    attempted addition of csv file with new content -- not implemented

--- lib/trip.rb
+++ lib/trip.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Trip
 
-  attr_reader :rider_id, :driver_id, :rating, :id, :date
+  attr_reader :rider_id, :driver_id, :rating, :id, :date, :cost, :duration_hhmmss
 
   def initialize(hash)
     @id = hash[:id]
@@ -11,6 +11,8 @@ class Trip
     @rider_id = hash[:rider_id]
     @date = hash[:date]
     @rating = hash[:rating]
+    @cost = hash[:cost]
+    @duration_hhmmss = hash[:duration_hhmmss]
   end
 
   def get_driver
@@ -42,6 +44,7 @@ class Trip
   def self.all(csv)
     my_file = CSV.open(csv)
     all_trip_info = []
+    prng = Random.new
     my_file.each do |line|
       trip_hash = {}
       trip_hash[:id] = line[0].to_i
@@ -49,6 +52,8 @@ class Trip
       trip_hash[:rider_id] = line[2].to_i
       trip_hash[:date] = line[3]
       trip_hash[:rating] = line[4].to_f if line[4].to_f >= 1 && line[4].to_f <= 5
+      trip_hash[:cost] = prng.rand(20.01).round(2).to_f
+      trip_hash[:duration_hhmmss] = "#{prng.rand(24).to_s.rjust(2, '0')}:#{prng.rand(60).to_s.rjust(2,'0')}:#{prng.rand(60).to_s.rjust(2,'0')}"
 
       all_trip_info << trip_hash
     end
@@ -64,3 +69,13 @@ class Trip
   end
 
 end
+
+all_trips = Trip.all("support/trips.csv")
+costs = []
+durations = []
+all_trips.each do |trip|
+  costs << trip.cost
+  durations << trip.duration_hhmmss
+end
+puts costs
+puts durations
