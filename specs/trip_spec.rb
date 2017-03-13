b5a5b5b849cbc022a2b1033883189336d3e7a5f1
Andrea Valliere <amvalliere@gmail.com>
2017-03-12 23:26:57 -0700

    Added tests for find_many_drivers and find_many_riders class methods for Trip class.

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -23,13 +23,22 @@ describe "Trip class" do
     end
   end
 
-  describe "self.find_many class method" do
-    # Not sure if this is going to work for both Rider and Driver or if I will need a separate method for both find_many_drivers and find_many_riders
-    it "Returns array of many Trip instances for given argument" do
-
+  describe "self.find_many_drivers class method" do
+    it "Returns array of many Trip instances for given driver" do
+      driver_trips = RideShare::Trip.find_many_drivers("1")
+      driver_trips.must_be_instance_of Array
+      driver_trips.length.must_equal 9
     end
   end
-# Need more tests here for self.all method
+
+  describe "self.find_many_riders class method" do
+    it "Returns array of many Trip instances for given rider" do
+      rider_trips = RideShare::Trip.find_many_riders("296")
+      rider_trips.must_be_instance_of Array
+      rider_trips.length.must_equal 3
+    end
+  end
+
   describe "self.all class method" do
     it "Returns array of all trips from CSV" do
       RideShare::Trip.all.must_be_instance_of Array, "Not an array"
@@ -41,8 +50,8 @@ describe "Trip class" do
 
       trips_array[1].trip_id.must_equal("1")
       trips_array[1].date.must_equal("2016-04-05")
-      trips_array[600].trip_id.must_equal("600")
-      trips_array[600].date.must_equal("2016-04-25")
+      trips_array[-1].trip_id.must_equal("600")
+      trips_array[-1].date.must_equal("2016-04-25")
     end
 
     it "Array retrieved by self.all retrieves same number of trips as is in CSV" do
