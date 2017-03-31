1a0935f4981152d9c0e4f18afbdaeb64c9e2aad6
EricaJCase <ericajcase@gmail.com>
2017-03-30 21:29:08 -0700

    all folders with trips.find_all complete

--- specs/trips_spec.rb
+++ specs/trips_spec.rb
@@ -26,13 +26,73 @@ describe "Trip" do
     end
 
     it "raises an argument error if driver_id, trip_id, or rider_id are missing" do
-      no_driver_trip.must_raise ArgumentError
-      no_rider_trip.must_raise ArgumentError
-      no_trip_id.must_raise ArgumentError
+      proc{no_driver_trip}.must_raise ArgumentError
+      proc{no_rider_trip}.must_raise ArgumentError
+      proc{no_trip_id}.must_raise ArgumentError
     end
 
     it "outputs a warning if :rating is outside acceptable range, but stores row with rating as nil" do
       proc{weird_rating}.must_output /.+/
+    end
+  end
+
+  describe "all" do
+    let(:all_trips) {Rideshare::Trip.all(:trip_id)}
+
+    it "returns a collection with right number of rows" do
+      all_trips.length.must_equal 595
+    end
+
+    it 'works for the first trip in the csv' do
+      all_trips.first.trip_id.must_equal 1
+      all_trips.first.driver_id.must_equal 1
+      all_trips.first.rider_id.must_equal 54
+      all_trips.first.date.must_equal "2016-04-05"
+      all_trips.first.rating.must_equal 5
+    end
+
+    it 'works for the last trip in the csv' do
+      all_trips.last.trip_id.must_equal 600
+      all_trips.last.driver_id.must_equal 61
+      all_trips.last.rider_id.must_equal 168
+      all_trips.last.date.must_equal "2016-04-25"
+      all_trips.last.rating.must_equal 3
+    end
+
+    describe "find_records" do
+      let(:first_trip) {Rideshare::Trip.find_records(:trip_id, 1)}
+
+      let(:last_trip) {Rideshare::Trip.find_records(:trip_id, 600)}
+
+      let(:imaginary_trip) {Rideshare::Trip.find_records(:trip_id, 601)}
+
+      # let(:double_driver) {Rideshare::Driver.find_records(':driver_id, 94)}
+
+
+      it 'works for the first trip in the csv' do
+        first_trip[0].trip_id.must_equal 1
+        first_trip[0].driver_id.must_equal 1
+        first_trip[0].rider_id.must_equal 54
+        first_trip[0].date.must_equal "2016-04-05"
+        first_trip[0].rating.must_equal 5
+      end
+
+      it 'works for the last trip in the csv' do
+        last_trip[0].trip_id.must_equal 600
+        last_trip[0].driver_id.must_equal 61
+        last_trip[0].rider_id.must_equal 168
+        last_trip[0].date.must_equal "2016-04-25"
+        last_trip[0].rating.must_equal 3
+      end
+
+      it "returns only one trip for first and last" do
+        last_trip.length.must_equal 1
+        first_trip.length.must_equal 1
+      end
+
+      it "returns empty array if the trip does not exist" do
+        imaginary_trip.must_be_empty
+      end
     end
   end
 end
